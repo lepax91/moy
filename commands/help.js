e39f28e1840db6e8531d7f87a6e6a8af64d9a89d
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-30 21:59:48 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+    var embed = new Discord.RichEmbed()
+    .addField("normální sfw příkazy", "meme");
+    .addField("nějaký to nsfw 😏", "ass, pussy, hentai");
+    message.channel.send(embed)
+    
+ });
+ 
+module.exports.help = {
+           name: 'help', 
+           aliases: ['halp']
+} 
