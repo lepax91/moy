bfa0be87f1f2ca0d56d988ad30f7dc09bde25439
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-30 17:53:13 +0100

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
  if(!message.channel.nsfw) return message.reply("Jseš si jistý, že si ve správném kanálu? 🤔");
  let reddit = [
                "hentai",
-               "thick_hentai"
+               "thick_hentai",
                "ecchi"
                        
                        
