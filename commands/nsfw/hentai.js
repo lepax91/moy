6618c14f8632209745d39a8d20504d0439d7a458
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-30 17:33:25 +0100

    Create hentai.js

---
+++ commands/nsfw/hentai.js
@@ -0,0 +1,27 @@
+const Botconfig = require("../botconfig.json");
+const superagent = require("superagent");
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+
+
+    if (!message.channel.nsfw) return message.channel.send("Šerife, Jseš si opravdu jistý že jsi v jistém kanále? 🤔");
+         // return 
+        superagent.get('https://nekos.life/api/v2/img/hentai')
+            .end((err, response) => {
+                var lewdembed = new Discord.RichEmbed()
+                    .setTitle(`Pěkný anime holky`)
+                    .setImage(response.body.url)
+                    .setColor("RANDOM")
+                    .setFooter("kuchař pepe vaří speciální semeno 👌", bot.user.displayAvatarURL)
+                    .setTimestamp();
+                message.channel.send(lewdembed);
+            });
+    
+}
+
+module.exports.help = {
+    name: "hentai",
+    aliases: []
+}
