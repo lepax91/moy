802b0c8293ae172e0c2227b8c4a6c435bd3664c6
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-30 16:58:42 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,56 @@
+const botconfig = require("./botconfig.json");
+//const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const moment = require("moment");
+const db = require("quick.db");
+const ms = require("ms");
+
+//const superagent = require("superagent")
+
+//JSON files
+let userData = JSON.parse(fs.readFileSync('./Storage/userData.json', 'utf8'));
+
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
+
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  
+ 
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let modR = "Moderator";
+  let adminR = "Administrator";
+  let args = messageArray.slice(1);
+  //let prefix = botconfig.prefix;
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+  
+	
+  
+ //WUTF IS DAT THING
+});
+bot.login(process.env.token);
