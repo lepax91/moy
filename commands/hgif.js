7524978712a91c315709111fa13291b19c162cce
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-31 18:38:25 +0100

    Create hgif.js

---
+++ commands/hgif.js
@@ -0,0 +1,37 @@
+const randomPuppy = require('random-puppy'); 
+
+module.exports.run = async (bot, message, args) => {
+ 
+ if(!message.channel.nsfw) return message.reply("Jseš si jistý, že si ve správném kanálu? 🤔");
+ let reddit = [
+               "HENTAI_GIF",
+               "AnimeHentaiGifs",
+               "hentaii_gif"
+               
+                       
+                       
+                       
+                       
+                      
+                       
+  ]
+
+let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+
+ message.channel.startTyping(); 
+
+randomPuppy(subreddit).then(async url => {
+                    await message.channel.send({
+                            files: [{ 
+                                  attachment: url, 
+                                  name: 'hentai.gif' 
+                          }]           
+                   }).then(() => message.channel.stopTyping()); 
+    }).catch(err => console.error(err)); 
+
+};
+
+ module.exports.help = {
+           name: 'hgif', 
+           aliases: ['hentaigif']
+} 
