c0bb02a0d92e53f2d4d18cee656d059b292d1ee6
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-31 14:18:58 +0100

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+  
+module.exports.run = async (bot, message, args) => {
+ 
+   let player = message.mentions.members.first() || message.member
+	   let user = player.user
+if(!user) return message.channel.send("You haven't selected/mentioned a user whose avatar you want to see."); 
+    let avatarEmbed = new Discord.RichEmbed()
+    .setAuthor(`${user.tag}`, `${user.displayAvatarURL}`)
+    .setTitle("Avatar")
+    .setImage(user.displayAvatarURL)
+    .setColor("#b70000");
+    return message.channel.send(avatarEmbed);
+     }
+});
+module.exports.help = {
+    name: "avatar",
+    aliases: ['']
+}
