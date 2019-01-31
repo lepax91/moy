8c4ba7901a5c396a7d9498c1f1366c2451feded6
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-31 11:25:49 +0100

    Create cat.js

---
+++ commands/cat.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const randompuppy = require('random-puppy');
+
+
+module.exports.run = async (bot, message, args) => {
+ 
+
+    let api = "cat"
+      randompuppy(api).then(api => {
+           const theirembed = new Discord.RichEmbed()
+	    .setAuthor("😺 Kočka | Vždy se zobrazí random obrázek.")
+            .setColor(0x2aff00)
+            .setImage(api)
+            .setFooter("Kuchař Pepe 👌")
+            .setTimestamp();
+      message.channel.send(theirembed)
+      })
+     }
+   
+
+ module.exports.help = {
+           name: 'cat',
+           aliases: ['cats']
+}    
