9dd4d72c18f41c9fa1565706d930c10eb41fbb4c
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-31 21:51:25 +0100

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -1,30 +1,16 @@
-const snekfetch = require('snekfetch');
-const token = require('../token.json').arcadia;
-
 module.exports.run = async (bot, message, args) => {
 
-    let target = message.mentions.users.first() || message.guild.members.get(args[0]).user || message.author;
-    let profilepic = target.avatarURL;
-    let url = `https://arcadia-api.xyz/api/v1/triggered?url=${profilepic}`;
+    let profilepic = message.author.avatarURL;
 
-    message.channel.startTyping();
-
-    snekfetch.get(url, {
-        headers: {
-            "Authorization": token
+    message.channel.send({
+        file: {
+            attachment: "https://cute-api.tk/v1/generate/triggered?url=" + profilepic,
+            name: "triggered.gif"
         }
-    }).then(async res => {
-        await message.channel.send({
-           files: [{
-               attachment: res.body,
-               name: `${target.tag}-triggered.gif`
-           }]
-        }).then(() => message.channel.stopTyping());
-    }).catch(err => console.error(err));
+    });
 
 };
 
 module.exports.help = {
-    name: 'triggered',
-    aliases: []
+    name: 'triggered'
 };
