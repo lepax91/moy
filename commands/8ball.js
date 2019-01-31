242d69ad72e9d0ea0db2b6749961507972f84a57
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-31 21:08:01 +0100

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args, tools) => {
+    
+    // Array of responses
+    let responses = [
+        'cože ne',
+        'samozřejmě že ano,
+        'nevim, nejsem aladin,
+        'nemel mrdky'
+    ]
+    
+    // Fetch a random item from the array
+    let fetched = responses[Math.floor(Math.random() * responses.length)];
+    
+    // Form Embed
+    const embed = new Discord.MessageEmbed()
+        .setColor(0xffffff)
+        .setFooter(fetched);
+    
+    // Send Embed
+    message.channel.send(embed);
+ 
+module.exports.help = {
+           name: '8ball',
+           aliases: ['']
+}           
