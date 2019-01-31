786c9394e3a16d02198d084791fdf6a36e74150f
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-31 09:15:41 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+   var embed = new Discord.RichEmbed()
+   .setAuthor("Help commands!", message.author.avatarURL)
+   .setColor("GREEN")
+   .setTimestamp()
+   .addField("všechny příkazy nsfw 😏", "ass, thigh, bbw, dick, pussy, hentai")
+   .addField("všechny funny příkazy 😂👌", "meme")
+   .setFooter(message.author.username + " Požadováno.");
+   
+   message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: "help",
+    aliases: ['halp']
+}
