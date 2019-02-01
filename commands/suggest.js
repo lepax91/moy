662187fd9a6ee8c86cf7726cc7d78e73be49c9ae
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-01 13:21:30 +0100

    Create suggest.js

---
+++ commands/suggest.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+
+    let suggestmessage = args.join(" ").slice(22);
+    let suggestchannel = message.mentions.channels.first();
+
+    if (!suggestchannel) {
+        return message.reply("Please Mention the channel!")
+    }
+
+    if (!suggestmessage) {
+        return message.reply("Plase Give Text To Suggestion Channel!")
+    }
+
+    let embed = new Discord.RichEmbed()
+        .addField("**SUGGESTION**", `${suggestmessage}`)
+        .setFooter(`Suggestion By ${message.author.tag}`)
+        .setTimestamp()
+    suggestchannel.send({
+        embed
+    }).then(msg => {
+        msg.react("✅").then(r => msg.react("❎"))
+    });
+
+
+    message.reply(`Your Suggestion is sended.`)
+return;
+
+module.exports.help = {
+    name: "suggest",
+    aliases: ['sug']
+}
