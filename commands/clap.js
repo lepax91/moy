d82bea68c1b90f6f27c004eac0289a5267a9b403
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-01 16:25:24 +0100

    Update clap.js

--- commands/clap.js
+++ commands/clap.js
@@ -1,8 +1,9 @@
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
-exports.run = (bot, message, args) => {
-    if (args.length < 1) return message.channel.send("**I need some text to clapify.** `ium clap <sentence>`")
-    message.channel.send(args.map(randomizeCase).join(':clap:'));
+module.exports.run = (bot, message, args) => {
+    
+    if (args.length < 1) return message.channel.send("Tlesknu jen když přidáš nějaký ten tExT")
+    message.channel.send(args.map(randomizeCase).join('👏'));
 }
 
 module.exports.help = {
