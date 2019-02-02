6af9f8faccf681533abcb127f8f9c7a0d50a92d6
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-02 15:17:01 +0100

    Update and rename hgif.js to feet.js

--- commands/feet.js
+++ commands/feet.js
@@ -4,17 +4,18 @@ const superagent = require("superagent");
 module.exports.run = async(bot, message, args) => {
     
     let {body} = await superagent
-    .get(`https://nekos.life/api/v2/img/Random_hentai_gif`);
+    .get(`https://nekos.life/api/v2/img/feet`);
     if (!message.channel.nsfw) return message.reply(" Jseš si jistý, že toto je správný kanál? 🤔");
   
     let hentaiEmbed = new Discord.RichEmbed()
-    .setTitle("hentai.gif")
+    .setTitle("Hentai Feet 🍆")
     .setImage(body.url)
+    .setColor("RANDOM")
     message.channel.send(hentaiEmbed);
 
 }
 
 module.exports.help = {
-    name: "hgif",
+    name: "feet",
     aliases: []
 }
