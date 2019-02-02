06d407abd33a2f601541ccc2b48dd7abcf891767
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-02 22:17:22 +0100

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -129,7 +129,8 @@ module.exports.run = async (bot, message, args) => {
                        let endEmbed = new Discord.RichEmbed()
                        .setAuthor(message.author.username, message.author.avatarURL)
                        .setTitle(title)
-                       .addField('🎉 Giveaway právě teď skončil!,`Výherce této soutěže je : ${gFilter}!')
+                       .setColor("RANDOM")
+                       .addField('🎉 Giveaway právě teď skončil!', `Výherce této soutěže je : ${gFilter}!`)
                        .setFooter(message.guild.name, message.guild.iconURL);
                        m.edit(endEmbed);
                      },duration);
