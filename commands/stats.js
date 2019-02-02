4223cc3e2dd9e3924e4c85b25285ce6ab5b03016
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-02 19:51:16 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -4,16 +4,23 @@ require("moment-duration-format");
 
 exports.run = (client, msg, args) => {
     const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-    msg.channel.send(`= STATISTICS =
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${Discord.version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
+    
+    
+    const embedStats = new Discord.RichEmbed()
+.setAuthor(bot.user.username)
+.setTitle("***BOT Stats***")
+.setColor("RANDOM")
+.addField("• Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+.addField("• Uptime",   `${duration}`, true)
+.addField("• Users",    `${client.users.size.toLocaleString()}`, true)
+.addField("• Servers",  `${client.guilds.size.toLocaleString()}`, true)
+.addField("• Channels", `${client.channels.size.toLocaleString()}`, true)
+.addField("• Discord.js" `v${Discord.version}`, true)
+.addField("• Node", `${process.version}`, {code: "asciidoc"});`, true)
 };
-
+message.channel.send(embedStats) });
+});
+    
 exports.conf = {
   enabled: true,
   guildOnly: false,
@@ -24,5 +31,4 @@ exports.help = {
   name: 'stats',
   aliases: [],
   description: 'Gives some useful bot statistics',
-  usage: 'stats'
-};
+  usage: 'sta
