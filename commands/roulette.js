3632772c8d71145c2ff4fee2e87896b83a0c4594
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-02 12:40:14 +0100

    Create roulette.js

---
+++ commands/roulette.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot, message, args) => {
+    randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
+    // console.log(randomNumber);
+    if(randomNumber==2){
+        message.reply("Died! 💀");
+    }else{
+        message.reply("Survived! 😃");
+    }
+}
+
+module.exports.help = {
+  name:"roulette"
+  aliases: []
+}
