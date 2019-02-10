4ba2c730351f107cb09d16b92e3ac289c1cb22f2
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-10 19:01:13 +0100

    Create kiss.js

---
+++ commands/kiss.js
@@ -0,0 +1,20 @@
+const discord = require('discord.js');
+const superagent = require('superagent')
+
+module.exports.run = async (bot, message, args, func) => {
+
+let kissUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!kissUser) return message.channel.send("You can't kiss me! *Even if I might like it...*")
+  const { body } = await superagent
+  .get(`https://nekos.life/api/v2/img/kiss`);
+
+  let kissEmbed = new Discord.RichEmbed()
+  .setTitle(`**Mwah! 😘**`)
+  .setDescription(`${message.author} kissed ${message.mentions.users.first()}!`)
+  .setImage(body.url)
+  .setColor("RANDOM")
+  .setFooter("Version: 1.2", bot.user.displayAvatarURL)
+  .setTimestamp();
+  message.channel.send(kissEmbed);
+
+}
