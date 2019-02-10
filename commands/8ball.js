5d21c9342b540fa6faf3b762623e7c45d22fec09
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-10 21:04:24 +0100

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let replies = ["Ne", "Ano", "Nemyslím si", "Zkus to znova později", "Spíše ano", "Raději ti to neřeknu", "Můžeš se na to spolehnout"]
+    let result = Math.floor((Math.random() * replies.length));
+
+    let question = args.slice().join(" ");
+
+    let embedz = new Discord.RichEmbed()
+        .setAuthor(message.author.username + " Ptá se: " + question)
+        .setColor("RANDOM")
+        .addField("Odpověď", "Zeptal se:" + message.author.tag + "\nOdpověď: " + replies[result] + "")
+
+    message.channel.send(embedz)
+};
+module.exports.help = {
+    name: "8ball",
+    aliases: []
+}
