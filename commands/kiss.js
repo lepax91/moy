c175e0f6b7a684e7bc93ae9a698e348dcc6176f6
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-10 20:40:13 +0100

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,24 +1,48 @@
-const discord = require('discord.js');
-const superagent = require('superagent')
-
-module.exports.run = async (bot, message, args, func) => {
-
-let kissUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!kissUser) return message.channel.send("😒 Hold on! You can't kiss me! **I will blush... 😳**")
+const Discord = require('discord.js');
+27
+const superagent = require("snekfetch");
+28
+​
+29
+module.exports.run = async (bot, message, args) => {
+30
+​
+31
+  let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+32
+  if(!hugUser) return message.channel.send("😒 You can't kiss me! **I will blush... 😳**")
+33
   const { body } = await superagent
+34
   .get(`https://nekos.life/api/v2/img/kiss`);
-
-  let kissEmbed = new Discord.RichEmbed()
+35
+​
+36
+  let hugEmbed = new Discord.RichEmbed()
+37
   .setTitle(`**Mwah! 😘**`)
+38
   .setDescription(`${message.author} kissed ${message.mentions.users.first()}!`)
+39
   .setImage(body.url)
+40
   .setColor("RANDOM")
+41
   .setFooter("Version: 1.2", bot.user.displayAvatarURL)
+42
   .setTimestamp();
-  message.channel.send(kissEmbed);
-
+43
+  message.channel.send(hugEmbed)
+44
+​
+45
 }
+46
 module.exports.help = {
+47
     name: "kiss",
+48
     aliases: []
+49
 }
+    
