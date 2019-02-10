f627a3966a671a7747b1330000cfba6090afa751
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-10 19:32:49 +0100

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,24 +1,24 @@
-const discord = require('discord.js');
-const superagent = require('superagent')
+const Discord = require('discord.js');
+const superagent = require("snekfetch");
 
-module.exports.run = (client, msg, args) => {
+module.exports.run = async (bot, message, args) => {
 
-  let slapUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!slapUser) return message.channel.send("😡 Ok, I'm angry once more, and I'm gonna give you a slap!")
+  let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!hugUser) return message.channel.send("😡 I'm Angry, Once more and I slap you!")
   const { body } = await superagent
   .get(`https://nekos.life/api/v2/img/slap`);
 
-  let slapEmbed = new Discord.RichEmbed()
-  .setTitle(`**Wack! 🖐**`)
+  let hugEmbed = new Discord.RichEmbed()
+  .setTitle(`**Pow! 👋**`)
   .setDescription(`${message.author} slapped ${message.mentions.users.first()}!`)
   .setImage(body.url)
   .setColor("RANDOM")
-  .setFooter("Version: 1.2", client.user.displayAvatarURL)
+  .setFooter("Version: 1.2", bot.user.displayAvatarURL)
   .setTimestamp();
-  message.channel.send(slapEmbed)
+  message.channel.send(hugEmbed)
 
 }
 module.exports.help = {
-           name: 'slap', 
-           aliases: []
-} 
+    name: "slap",
+    aliases: []
+}
