4fe61e628c0ad4feabbf288e9673c6050c248520
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-10 18:39:50 +0100

    Update howgay.js

--- commands/howgay.js
+++ commands/howgay.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix) => {
 
+    let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!hugUser) return message.channel.send("🤔 | Ok, i'm bisexual stop ok?")
     let gay = Math.round(Math.random() * 100);
-
     let gayembed = new Discord.RichEmbed()
         .setColor("#f442d4")
         .setTitle(`👨‍❤️‍💋‍👨 ${message.author.username} is ${gay}% gay! 👨‍❤️‍💋‍👨`);
