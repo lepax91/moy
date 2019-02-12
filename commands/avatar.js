f73925f2931a9f0f5f9c1ad33dde6d7d99099e6e
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-12 16:12:34 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const sm = require('string-similarity');
 
-exports.run = (client, message, args, tools) => {
+exports.run = (message, args, tools) => {
   
   let members = [];
   let indexes = [];
