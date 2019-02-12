f3760c9aa0d77b22bf49e9b0cbdfd4caee15ccca
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-12 17:36:03 +0100

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -4,7 +4,7 @@ const superagent = require("snekfetch");
 module.exports.run = async (bot, message, args) => {
   
   if(args[0] == "help"){
-        message.reply("❓ Example: `.kiss <@user>");
+        message.reply("❓ Example: `.kiss <@user>`");
         return;
   };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
