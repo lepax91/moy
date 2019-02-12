fc81c2827ff960a54ccb12b5c1e8f19258b777df
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-12 16:35:16 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,11 +6,12 @@ module.exports.run = async (bot, message, args) => {
     if(!message.content.startsWith(PREFIX)) return;
 
     if(args[0] == "help"){
-        message.send("❓ | `.avatar <@user/bot>`");
+        message.reply("❓ Example: .avatar <@user/bot>");
         return;
-    };  
-    let user = message.mentions.users.first
-    
+    };
+    let msg = await message.channel.send("⏰ | Wait a second..");
+    let target = message.mentions.users.first() || message.author;
+
         message.channel.send({embed: {
         color: 1339135,
         image: {
@@ -19,19 +20,18 @@ module.exports.run = async (bot, message, args) => {
         timestamp: new Date(),
         footer: {
           icon_url: bot.user.displayAvatarURL ,
-          text: "Version: 1.2",
+          text: "Dot | Version: 1.2",
         },
-        author: {
+        author: { 
             icon_url: message.guild.iconURL,
-            name: `${usernameUser.tag}'s avatar`,
+            name: "⚫ Here is your avatar, you wanted",
           }
         }});
 
         msg.delete();
 }
 module.exports.help = {
-    name: "avatar",
-    aliases: [], 
-    description: "Displays avatar."
-	
+    name: "avatar",	
+    aliases: []
+		
 }
