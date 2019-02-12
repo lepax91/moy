91251732f975754cbfb7e2ee68da9ba15680e9b5
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-12 21:21:50 +0100

    Update tickle.js

--- commands/tickle.js
+++ commands/tickle.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         return;
   };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!hugUser) return message.channel.send("😒 Do you try tickle me? Sorry but no..")
+  if(!hugUser) return message.channel.send("**❕ | First u need type on any channel command: `.kiss help`!**")
   const { body } = await superagent
   .get(`https://nekos.life/api/v2/img/tickle`);
 
