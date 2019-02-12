de2955d2d1493f77f7fe2836bd64f0a888de594c
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-12 17:30:53 +0100

    Update tickle.js

--- commands/tickle.js
+++ commands/tickle.js
@@ -2,7 +2,11 @@ const Discord = require('discord.js');
 const superagent = require("snekfetch");
 
 module.exports.run = async (bot, message, args) => {
-
+  
+  if(args[0] == "help"){
+        message.reply("❓ Example: `.tickle <@user>");
+        return;
+  };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!hugUser) return message.channel.send("😒 Do you try tickle me? Sorry but no..")
   const { body } = await superagent
