f55729cd9b27e55d067227e8bd0472cebc57efdc
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-12 17:30:28 +0100

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -2,7 +2,11 @@ const Discord = require('discord.js');
 const superagent = require("snekfetch");
 
 module.exports.run = async (bot, message, args) => {
-
+  
+  if(args[0] == "help"){
+        message.reply("❓ Example: `.slap <@user>");
+        return;
+  };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!hugUser) return message.channel.send("😒 You can't slap me!")
   const { body } = await superagent
