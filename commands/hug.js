17967138c4d3ec1c9eb45d60cbe23bb08fc1c732
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-12 21:20:55 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         return;
   };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!hugUser) return message.channel.send(":x: | Wait.. You can't hug me why? I don't know.. 😂👌")
+  if(!hugUser) return message.channel.send("**❕ | First u need type on any channel command: `.hug help`!**")
   const { body } = await superagent
   .get(`https://nekos.life/api/v2/img/hug`);
 
