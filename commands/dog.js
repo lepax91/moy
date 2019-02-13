6f6ceb9a416342fcd86acb2d199b0c37deb0ba22
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-13 15:04:22 +0100

    Create dog.js

---
+++ commands/dog.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js"),
+    superagent = require("superagent");
+module.exports.run = async (client, message, args) => {
+    let {
+        body
+    } = await superagent
+        .get(`https://random.dog/woof.json`);
+    const dogembed = new Discord.RichEmbed()
+        .setTitle("Aww... Doggo!")
+        .setColor("RANDOM")
+        .setImage(body.url)
+    message.channel.send(dogembed);
+
+}
