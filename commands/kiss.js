15f681e5c5273551c5f482ae103180cfa8e461bc
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-02-13 13:50:17 +0100

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const superagent = require("snekfetch");
-s 
+
 module.exports.run = async (bot, message, args) => {
   
   if(args[0] == "help"){
         message.reply("❓ Example: `.kiss <@user>`");
         return;
-  
+  };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!hugUser) return message.channel.send("**❕ | First u need type on any channel command: `.kiss help`!**")
   const { body } = await superagent
