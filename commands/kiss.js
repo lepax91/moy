3fa4438e619b62f902f4b77128a525bdef30295a
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-01 19:40:49 +0100

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -4,20 +4,20 @@ const superagent = require("snekfetch");
 module.exports.run = async (bot, message, args) => {
   
   if(args[0] == "help"){
-        message.reply("❓ Example: `.kiss <@user>`");
+        message.reply("❓ Například: `.kiss <@user>`");
         return;
   };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!hugUser) return message.channel.send("**❕ | First u need type on any channel command: `.kiss help`!**")
+  if(!hugUser) return message.channel.send("**❕ | Prvně musíš napsat do jakéhokoliv kanálu: `.kiss help`!**")
   const { body } = await superagent
   .get(`https://nekos.life/api/v2/img/kiss`);
 
   let hugEmbed = new Discord.RichEmbed()
   .setTitle(`**Mwah! 😘**`)
-  .setDescription(`${message.author} kissed ${message.mentions.users.first()}!`)
+  .setDescription(`${message.author} dal pusu ${message.mentions.users.first()}!`)
   .setImage(body.url)
   .setColor("RANDOM")
-  .setFooter("Version: 1.2", bot.user.displayAvatarURL)
+  .setFooter("Verze: 1.2", bot.user.displayAvatarURL)
   .setTimestamp();
   message.channel.send(hugEmbed)
 
