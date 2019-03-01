f58a32e9c8ac40585a7f8799399590a3ed332e7e
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-01 19:42:33 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -4,20 +4,20 @@ const superagent = require("snekfetch");
 module.exports.run = async (bot, message, args) => {
   
   if(args[0] == "help"){
-        message.reply("❓ Example: `.hug <@user>`");
+        message.reply("❓ Například: `.hug <@user>`");
         return;
   };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!hugUser) return message.channel.send("**❕ | First u need type on any channel command: `.hug help`!**")
+  if(!hugUser) return message.channel.send("**❕ | Prvně musíš napsat do jakéhokoliv kanálu: `.hug help`!**")
   const { body } = await superagent
   .get(`https://nekos.life/api/v2/img/hug`);
 
   let hugEmbed = new Discord.RichEmbed()
   .setTitle(`**Hugs?**`)
-  .setDescription(`${message.author} hugged ${message.mentions.users.first()}!`)
+  .setDescription(`${message.author} obejmul ${message.mentions.users.first()}!`)
   .setImage(body.url)
   .setColor("RANDOM")
-  .setFooter("Version: 1.2", bot.user.displayAvatarURL)
+  .setFooter("Verze: 1.2", bot.user.displayAvatarURL)
   .setTimestamp();
   message.channel.send(hugEmbed)
 
