763329f4ed461f2819cb56201f384885b4587232
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-01 19:47:33 +0100

    Update tickle.js

--- commands/tickle.js
+++ commands/tickle.js
@@ -4,20 +4,20 @@ const superagent = require("snekfetch");
 module.exports.run = async (bot, message, args) => {
   
   if(args[0] == "help"){
-        message.reply("❓ Example: `.tickle <@user>`");
+        message.reply("❓ Například: `.tickle <@user>`");
         return;
   };
   let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!hugUser) return message.channel.send("**❕ |  First u need type on any channel command: `.tickle help`!**")
+  if(!hugUser) return message.channel.send("**❕ |  Prvně musíš do jakéhokoliv kanálu napsat: `.tickle help`!**")
   const { body } = await superagent
   .get(`https://nekos.life/api/v2/img/tickle`);
 
   let hugEmbed = new Discord.RichEmbed()
-  .setTitle(`**Tickle Tickle! 🤣**`)
-  .setDescription(`${message.author} tickled ${message.mentions.users.first()}!`)
+  .setTitle(`**Lehtání 🤣**`)
+  .setDescription(`${message.author} lechtal ${message.mentions.users.first()}!`)
   .setImage(body.url)
   .setColor("RANDOM")
-  .setFooter("Version: 1.2", bot.user.displayAvatarURL)
+  .setFooter("Verze: 1.2", bot.user.displayAvatarURL)
   .setTimestamp();
   message.channel.send(hugEmbed)
 
