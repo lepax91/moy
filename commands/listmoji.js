9322a6516412abbf6483a443ef769b3e63fed51a
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-05 20:50:39 +0100

    Create listmoji.js

---
+++ commands/listmoji.js
@@ -0,0 +1,15 @@
+const {RichEmbed} = require('discord.js');
+
+exports.run = (client, message) => {
+  const emojiList = message.guild.emojis.map(e => e.toString()).join(' ');
+  const embed = new RichEmbed()
+    .setColor(0xF18E8E)
+    .setTitle('Emoji List')
+    .setDescription(`${message.author.username}, Tyto emotes jsou na serveru:\n\n${emojiList}\n\n**${message.guild.emojis.array().length}** jsou celkem emotes.`);
+  message.channel.send({embed});
+};
+exports.help = {
+  name: 'listmoji',
+  description: 'Sends a list of the server\'s emojis',
+  usage: 'listmoji',
+  type: 'server'
