6c48a26b7aadb3dfed8c55dd009226e6ef2c7e8d
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-05 21:07:03 +0100

    Update listmoji.js

--- commands/listmoji.js
+++ commands/listmoji.js
@@ -3,9 +3,9 @@ const {RichEmbed} = require('discord.js');
 exports.run = (client, message) => {
   const emojiList = message.guild.emojis.map(e => e.toString()).join(' ');
   const embed = new RichEmbed()
-    .setColor(0xF18E8E)
-    .setTitle('Emoji List')
-    .setDescription(`${message.author.username}, Tyto emotes jsou na serveru:\n\n${emojiList}\n\n**${message.guild.emojis.array().length}** jsou celkem emotes.`);
+    .setColor("RANDOM")
+    .setTitle('Emotes ve tvém serveru')
+    .addField(`${message.author.username} Tyto emotes jsou na serveru:\n\n${emojiList}\n\n**${message.guild.emojis.array().length}** emotes jsou celkem na serveru.`);
   message.channel.send({embed});
 }
 exports.help = {
