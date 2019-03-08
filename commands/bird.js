57e32e450ee27f2ce9eec59775e106103ebff768
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-08 23:18:05 +0100

    Update bird.js

--- commands/bird.js
+++ commands/bird.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js'),
 const request = require('superagent');
 module.exports.run = async (client, message, args) => {
-     let { body } = await request.get(`https://api-to.get-a.life/birbimg`);
-    let birdembed = new Discord.RichEmbed()
+    let { body } = await request.get(`https://api-to.get-a.life/birbimg`);
+    let Discord = new Discord.RichEmbed()
       .setTitle('Birdy 🐦')
     .setColor("#7289da")
     .setImage(body.link); 
