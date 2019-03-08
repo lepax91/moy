5f206134178853d3aaf0b7b1184dfcb0423ee3f2
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-08 23:19:52 +0100

    Update bird.js

--- commands/bird.js
+++ commands/bird.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js'),
-const request = require('superagent');
+request = require('superagent');
 module.exports.run = async (client, message, args) => {
     let { body } = await request.get(`https://api-to.get-a.life/birbimg`);
     let Discord = new Discord.RichEmbed()
