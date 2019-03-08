54445b43c7dde34e5e2d47103d94681a5a613709
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-08 23:08:58 +0100

    Create bird.js

---
+++ commands/bird.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js'),
+    superagent = require('superagent');
+module.exports.run = async (client, message, args) => {
+     let { body } = await request.get(`https://api-to.get-a.life/birbimg`);
+    let birdembed = new Discord.RichEmbed()
+      .setTitle('Birdy 🐦')
+    .setColor("#7289da")
+    .setImage(body.link); 
+    message.channel.send(birdembed);
+}
+module.exports.help = {
+    name: "bird",
+    aliases: []
+}
