130f4bf22bca7ea6900e256ec324530b86f1cd88
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-08 23:06:52 +0100

    Create bunny.js

---
+++ commands/bunny.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js"),
+superagent = require("superagent");
+    
+module.exports.run = async (client, message, args) => {
+      let { body } = await request.get('https://api-to.get-a.life/redpandaimg');
+    let redpandaembed = new Discord.RichEmbed()
+    .setTitle('Red Panda 🐼')
+    .setColor(0x7289da) 
+    .setImage(body.link);
+    message.channel.send(redpandaembed);
+}
+module.exports.help = {
+    name: "rpanda",
+    aliases: []
+}
