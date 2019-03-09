6a10cf1e20b522cbc54f03a642530cdcc969f5bb
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-09 12:05:45 +0100

    Create suggest.js

---
+++ commands/suggest.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+    var canal = message.guild.channels.find('name', 'suggest');
+    let embed = new Discord.RichEmbed()
+        .addField("Navrhnout", `${suggestmessage}`)
+        .setFooter(`Návrh: ${message.author.tag}`)
+        .setTimestamp()
+    suggestchannel.send({
+        embed
+    }).then(msg => {
+        msg.react("✅").then(r => msg.react("❎"))
+    });
+
+
+    message.reply(`:white_check_mark: **| Váš návrh byl úspěšně poslán.**`)
+    return;
+}
+exports.help = {
+    name: "suggest",
+    aliases: []
+}
