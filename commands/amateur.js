68635f7f8eb0e8330224ca17f33d048f6eca7e3c
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-09 17:39:33 +0100

    Create amateur.js

---
+++ commands/amateur.js
@@ -0,0 +1,28 @@
+const randomnsfw = require('random-puppy');
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+ 
+	   if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
+     let nsfwreddits = [
+        'RealGirls',
+        'amateur',
+        'gonewild'
+    
+    ]
+    let api = nsfwreddits[Math.round(Math.random() * (nsfwreddits.length - 1))];
+      randomnsfw(api).then(api => {
+           const theirembed = new Discord.RichEmbed()
+            .setTitle("Amateur")
+            .setColor("RANDOM")
+            .setImage(api)
+            .setFooter("Version: 1.2")		
+            .setTimestamp();
+      message.channel.send(theirembed);
+      })
+}
+module.exports.help = {
+    name: "amateur",
+    aliases: []
+} 
