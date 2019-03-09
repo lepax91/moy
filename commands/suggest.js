8cf3275236491c163beb21404e32fd447edcd6ac
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-09 12:17:05 +0100

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,16 +1,13 @@
 const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
-    let suggestmessage = args.join(" ").slice(22);
     var canal = message.guild.channels.find('name', 'suggest');
     let embed = new Discord.RichEmbed()
         .addField("Navrhnout", `${suggestmessage}`)
         .setFooter(`Návrh: ${message.author.tag}`)
         .setColor("RANDOM")
         .setTimestamp()
-    suggestchannel.send({
-        embed
-    }).then(msg => {
+    canal.send({embed}).then(msg => {
         msg.react("✅").then(r => msg.react("❎"))
     });
 
