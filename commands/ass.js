177dbcb3b474159b505a4315f165d874689ed8f6
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-10 19:13:44 +0100

    Update ass.js

--- commands/ass.js
+++ commands/ass.js
@@ -2,8 +2,12 @@ const Discord = require('discord.js');
 const { get } = require('snekfetch');
 
 exports.run = async (client, message) => {
-
-    if (!message.channel.nsfw) return message.channel.send(":underage: Hey my dude! This is not NSFW channel.");
+ if (!message.channel.nsfw)
+  var ozelmesajuyari = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setTitle(':underage: **_Tento příkaz je povolen jen kde je označen NSFW kanál!_**')
+  .setImage('https://cdn.discordapp.com/attachments/542778827051499564/554356353641086977/20190310_183407.png')
+  return message.channel.send(ozelmesajuyari)  	
     const { body } = await get("https://nekobot.xyz/api/image?type=ass");
 
     const embed = new Discord.RichEmbed()
