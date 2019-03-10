ebe6715ada850e3d89685ee36bb2d833a63844c2
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-10 19:25:18 +0100

    Create hanal.js

---
+++ commands/hanal.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const { get } = require('snekfetch');
+
+exports.run = async (client, message) => {
+if (!message.channel.nsfw)
+  var ozelmesajuyari = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setTitle(':underage: **_Tento příkaz je povolen jen kde je označen NSFW kanál!_**')
+  .setImage('https://cdn.discordapp.com/attachments/542778827051499564/554356353641086977/20190310_183407.png')
+  return message.channel.send(ozelmesajuyari)  	
+    const { body } = await get("https://nekobot.xyz/api/image?type=hentai_anal");
+
+    const embed = new Discord.RichEmbed()
+        .setTitle('Hentai Anal')
+        .setURL(body.message)
+        .setColor("RANDOM")
+        .setImage(body.message)
+        .setFooter(`Version: 1.2`
+        .setTimestamp()
+    message.channel.send(embed);
+}
+exports.help = {
+    name: "hanal",
+    aliases: []
+}
