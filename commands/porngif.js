e9bf2ce0b703300d0e80456f28818eaf6da3fd81
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-11 18:23:42 +0100

    Create porngif.js

---
+++ commands/porngif.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const { get } = require('snekfetch');
+
+exports.run = async (client, message) => {
+
+    if (!message.channel.nsfw) return message.channel.send(":underage: **_Tento příkaz je povolen jen kde je označen NSFW kanál!_**");
+    const { body } = await get("https://nekobot.xyz/api/image?type=pgif");
+
+    const embed = new Discord.RichEmbed()
+        .setTitle('PornGif')
+        .setURL(body.message)
+        .setColor(0x619232)
+        .setImage(body.message)
+        .setFooter(`Požadováno od: ${message.author.tag} | Version: 1.2`)
+
+    message.channel.send(embed);
+}
+exports.help = {
+    name: "pgif",
+    aliases: []
+}
