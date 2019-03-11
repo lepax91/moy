7599b86632eaf19dbb68c63abe4c07c3cb5738f9
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-11 18:11:50 +0100

    Create math.js

---
+++ commands/math.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const math = require("mathjs");
+
+module.exports.run = async (bot, message, args) => {
+    if(!args[0]) return message.channel.send(":x: | Prosím zadej příklad.")
+
+    let resp;
+    try {
+        resp = math.eval(args.join(' '));
+    } catch (e) {
+        return message.channel.send(":x: | Neplatný, Prosím zadej platný příklad.");
+    }
+
+    const embed = new Discord.RichEmbed()
+    .setColor(0xffffff)
+    .setTitle(`**_Matematika_**`)
+    .addField('Příklad', `\`\`\`js\n${args.join(" ")}\`\`\``)
+    .addField('Odpověď', `\`\`\`js\n${resp}\`\`\``);
+
+    message.channel.send(embed);
+
+}
+
+
+module.exports.help = {
+    name: "math"
+  }
