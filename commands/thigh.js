2f11d8d5d422ec4f57c10b4f848854a39121752b
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-11 18:09:01 +0100

    Create thigh.js

---
+++ commands/thigh.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const { get } = require('snekfetch');
+
+exports.run = async (client, message) => {
+ if (!message.channel.nsfw) return message.channel.send(":underage: **_Tento příkaz je povolen jen kde je označen NSFW kanál!_**")
+    const { body } = await get("https://nekobot.xyz/api/image?type=ass");
+
+    const embed = new Discord.RichEmbed()
+        .setTitle('Thigh')
+        .setURL(body.message)
+        .setColor("RANDOM")
+        .setImage(body.message)
+        .setFooter(`Version: 1.2`)
+        .setTimestamp()
+    message.channel.send(embed);
+}
+exports.help = {
+    name: "thigh",
+    aliases: []
+}
