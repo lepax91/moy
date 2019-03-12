5e6ef18b10e741e427135db7d2d8e2fbd78958eb
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-12 23:16:37 +0100

    Update snapchat.js

--- commands/snapchat.js
+++ commands/snapchat.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
             .setTitle("Snapchat")
             .setColor("RANDOM")
             .setImage(api)
-            .setFooter("Version: 1.2")
+            .setFooter("Požadováno od: ${message.author.tag} | Verze: 1.5")
             .setTimestamp();
       message.channel.send(theirembed);
       })
