7bf443f1be91950b6acdae2d7e40b9a158bf17ae
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-12 23:17:50 +0100

    Update trap.js

--- commands/trap.js
+++ commands/trap.js
@@ -16,8 +16,7 @@ module.exports.run = async (bot, message, args) => {
             .setTitle("Traps")
             .setColor("RANDOM")
             .setImage(api)
-            .setFooter("Version: 1.2")
-            .setTimestamp();
+            .setFooter("Požadováno od: ${message.author.tag} | Verze: 1.5")
       message.channel.send(theirembed);
       })
 }
