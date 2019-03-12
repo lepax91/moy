54cb249071e99aa764f0a100f249534a417a783b
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-12 23:17:07 +0100

    Update thigh.js

--- commands/thigh.js
+++ commands/thigh.js
@@ -10,7 +10,7 @@ exports.run = async (client, message) => {
         .setURL(body.message)
         .setColor("RANDOM")
         .setImage(body.message)
-        .setFooter(`Version: 1.2`)
+        .setFooter(`Požadováno od: ${message.author.tag} | Verze: 1.5`)
         .setTimestamp()
     message.channel.send(embed);
 }
