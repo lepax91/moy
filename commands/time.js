dc753bed2cbe69e2423008c740711db245a21576
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-13 16:17:45 +0100

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-var today = new Date()
-let Day = today.toString().split(" ")[0].concat("day");
-let Month = today.toString().split(" ")[1]
-let Year = today.toString().split(" ")[3]
+var dnes = new Date()
+let Den = dnes.toString().split(" ")[0].concat("den");
+let Měsíc = dnes.toString().split(" ")[1]
+let Rok = dnes.toString().split(" ")[3]
 const embed = new Discord.RichEmbed()
       .setColor(`RANDOM`)
-.addField("Today is", `\`${Day}\` ,\`${Month}\` ,\`${Year}\`\n\`Time of day:\` \`${today.toString().split(" ")[4]}\``)
+.addField("Dneska je", `\`${Den}\` ,\`${Měsíc}\` ,\`${Rok}\`\n\~`Den:`\` \`${dnes.toString().split(" ")[4]}\``)
 message.channel.send({ embed })
     message.react("🕰")   
 };
 module.exports.help = {
     name: "time",
     aliases: []
-}
+}      
