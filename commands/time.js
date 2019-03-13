20fcf94238c6b0c98111bd5aae15af6df6764059
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-13 16:04:00 +0100

    Create time.js

---
+++ commands/time.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+var today = new Date()
+let Day = today.toString().split(" ")[0].concat("day");
+let Month = today.toString().split(" ")[1]
+let Year = today.toString().split(" ")[3]
+const embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+.addField("Today is", `\`${Day}\` ,\`${Month}\` ,\`${Year}\`\n\`Time of day:\` \`${today.toString().split(" ")[4]}\``)
+message.channel.send({ embed })
+    message.react("🕰")   
+};
+module.exports.help = {
+    name: "time",
+    aliases: []
+}
