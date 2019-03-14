04b25a2c62cc48d2adcc9d0f4aa68c3c0bedf63e
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-14 21:58:14 +0100

    Create panda.js

---
+++ commands/panda.js
@@ -0,0 +1,23 @@
+const superagent = require("snekfetch");
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+    superagent.get('http://random.birb.pw/tweet/')
+        .end((err, response) => {
+         const catembed = new Discord.RichEmbed()
+        .setTitle('🐼 Panda | To je roztomilý!')
+        .setColor("RANDOM")
+        .setImage(response.body.url)
+    message.channel.send(catembed);
+});
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "panda",
+    aliases: []
+};
