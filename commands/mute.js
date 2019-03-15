24e1fd4f0b6b41026a5862974436e35378a6d0de
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-15 14:48:36 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -2,24 +2,44 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     if (message.member.permissions.has("MUTE_MEMBERS") == true) {
         let member = message.mentions.members.first();
     if(!member)
-      return message.reply(":x: **_Označ prosím uživatele, který je na tomto serveru._**");
+      return message.reply("Please mention a valid member of this server");
     var guild = client.guilds.find(g => g.id == message.guild.id);
     var mutedRole = message.guild.roles.find(r => r.name == "Muted");
-    if (mutedRole == null)
-    	return;
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "Žádný důvod nebyl nalezen";
-    if (member.roles.find(r => r.id == mutedRole.id) == null) {
-      return message.reply(`:x: Nemůžu unmutnout toho, co nemá Muted roli!`);
-    }
-    await member.roles.remove(mutedRole)
-      .catch(error => {
-        return message.reply(`:x: **_ Uživatele ${message.author} nemohu najít, zjisti si jestli je na serveru.`);
+    if (mutedRole == null) {
+    	guild.createRole({name: "Muted", color: "GRAY"
+    		, permissions: ["READ_MESSAGE_HISTORY"]
+    	, mentionable: false}, "For muting people")
+    	.then(role => {
+    		mutedRole = role;
+    		for (var gc in guild.channels.values()) {
+    			if (gc.type == "text") {
+    				gc.overwritePermissions(role, {
+        				'SEND_MESSAGES': false,
+        				'ADD_REACTIONS': false
+        			}, "To mute people");
+    			} else {
+    				gc.overwritePermissions(role, {
+        				'SPEAK': false
+        			}, "To mute people")
+    			}
+    		}
       });
-    message.reply(`${member.user.tag} byl unmuted Moderátorem: ${message.author.tag} Protože: ${reason}`);
-    client.modlog(message.guild.id, "Unmute", message.author.tag, member.user.tag, důvod, "00FF00")
+    } else {
+      mutedRole = mutedRole;
+    }
+    if (member.roles.find(r => r.id == mutedRole.id) != null) {
+      return message.reply(`:x: **_Nemůžu ho mutnout, omlouvám se._**`);
+    }
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    await member.roles.add(mutedRole)
+      .catch(error => {
+        return message.reply(`Uživatele jménem ${message.author} nemůžu současně mutnout.`);
+      });
+    message.reply(`:white_check_mark: Mute byl úspěšný! | ${member.user.tag} byl muted Moderátorem ${message.author.tag} protože: ${reason}`);
+    client.modlog(message.guild.id, "Mute", message.author.tag, member.user.tag, reason, "FFA500")
   } else {
-    message.channel.send(":x: **_Na tuto akci potřebuješ permissions, jinak to nejde!_**");
+    message.channel.send(":x: **_Na tuto akci, nemáš žádnou pravomoc!_**");
   }
 };
   
@@ -30,9 +50,9 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   };
   
   exports.help = {
-    name: "unmute",
+    name: "mute",
     aliases: [],
     category: "Moderation",
-    description: "Unmute a user",
-    usage: "unmute @user reason"
+    description: "Mute a user",
+    usage: "mute @user reason"
   };
