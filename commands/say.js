3561a5cf13a2c46133fd8d075cb8f5c98fa510c8
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-03-25 18:48:23 +0100

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let botmessage = args.join(" ");
+  message.channel.send(botmessage);
+}
+
+module.exports.help = {
+  name: "say",
+  aliases: []
+}
