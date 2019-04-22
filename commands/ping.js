d9326f54c70c1b9150a727f29370584c3914451b
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-22 20:50:53 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,16 +2,13 @@ const Discord = require('discord.js')
 
 exports.run = async (client, message, args, color) => {
 
-    let start = Date.now(); message.channel.send(message.channel.id, 'Pong! ').then(message => { 
     let diff = (Date.now() - start); 
-    let API = (client.ping).toFixed(2)
         
         let embed = new Discord.RichEmbed()
         .setColor("RANDOM")
         .setFooter(`Ping: ${diff}ms`)
-        message.edit(embed);
-      
-    });
+        message.send(embed)
+     
 
 }
 exports.help = {
