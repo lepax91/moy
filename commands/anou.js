605b6244ead5e1b68e09331d9ecdbea2cd5d6291
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-22 16:48:20 +0200

    Update anou.js

--- commands/anou.js
+++ commands/anou.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
 
-exports.run = async (client, message) => {
-		   const color = args[0]
+exports.run = async (client, message) => {		
 		   const text = args.slice(1).join(" ");
        if (message.author.id != 417403958814965771) return message.reply("Tento příkaz můžou používat pouze jen ty co jsou v Dot Development Project.")
 		   if (text.length < 1) return message.channel.send("kámo ty seš dobrej kokot");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
-		   .setColor("0x" + color)
-		   .setTitle("Important Announcement:")
+		   .setColor("RANDOM")
+		   .setTitle("Dot")
 		   .setDescription(text);		
 		   message.channel.send({embed})
 }
