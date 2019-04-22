12203c3d391f4c1761d347c2d67a9584ae1d28f7
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-22 18:58:23 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,9 +3,10 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
   if (args == 0) return message.channel.send('**_Chybí mi tu nějaká otázka 🤔_**')
 
-  let embed = new Discord.RichEmbed()
-    .setTitle(`Hlasovaní vytvořil: ${message.author.username}`)
-    .setDescription(`${args}`.split(',').join(' '));
+  let embed = new Discord.RichEmbed()    
+    .setColor("RANDOM")
+    .setFooter(`Vytvořil(a): ${message.author.username}`)  
+    .setDescription(`${args}`.split(',').join(' '));    
 
   return message.channel.send(embed)
     .then(function (message, str) {
