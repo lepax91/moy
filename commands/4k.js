2a3b429b7413396e2aece0cc194c4f749c2dfec4
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-22 22:09:15 +0200

    Update 4k.js

--- commands/4k.js
+++ commands/4k.js
@@ -2,9 +2,13 @@ const Discord = require('discord.js');
 const { get } = require('snekfetch');
 
 exports.run = async (client, message) => {
-  if (!message.channel.nsfw) return message.channel.send(":underage: **_Tento příkaz je povolen jen kde je označen NSFW kanál!_**")	
+    if (!message.channel.nsfw) {
+    return message.channel.send(new Discord.RichEmbed()
+      .setColor("RED")
+      .setDescription(":underage: Tento příkaz je povolen jen kde je označen NSFW kanál.")
+    );
+  }
     const { body } = await get("https://nekobot.xyz/api/image?type=4k");
-
     const embed = new Discord.RichEmbed()
         .setTitle('4k')
         .setURL(body.message)
