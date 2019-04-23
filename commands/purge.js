acb9d1dd67e942625690ed63777446c5aa1484f3
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-23 15:17:59 +0200

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,11 +2,11 @@ var Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("no");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":warning: **Na tento příkaz musíš mít práva!**");
+  if(!args[0]) return message.channel.send("......");
   message.channel.bulkDelete(args[0]).then(() => {
   var cleanEmbed = new Discord.RichEmbed()            
-            .setDescription(`**:white_check_mark: Smazáno **${args[0]}** zpráv!**`)
+            .setDescription(`**:white_check_mark: Smazáno ${args[0]} zpráv!**`)
             .setFooter('Požadováno: ' + message.author.tag, message.author.avatarURL)
             .setTimestamp()
             .setColor('#ffffff');
