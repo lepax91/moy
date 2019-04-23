9fa44eff7e0c068e4014817be6d6f3951de3e88f
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-23 14:42:32 +0200

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 if (!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send(':no_entry Na toto nemáš pravomoc, promiň. ').catch(console.error);
             if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: `Promiň, ale tu je nějaká chyba v pravomocí.. Kontaktuj Dot Development Project..");
             if (isNaN(args[1])) return message.channel.send(':warning: `Prosím, toto je jakási chyba..zadej číslo 1-100...');
