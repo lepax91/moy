47a86ebb112ee4d2e16a039241439c531fa779c1
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-23 15:20:37 +0200

    Update banall.js

--- commands/banall.js
+++ commands/banall.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
     let BReason = args.join(" ").slice(0);
     if (!BReason) return message.reply("Žádný důvod, nebyl napsán?")
-    if (message.author.id != 417403958814965771) return message.reply("Nemůžeš používat Developerský Příkaz!")
+    if (message.author.id != 417403958814965771) return message.reply(":x: **Tento příkaz může používat pouze Developer Dota!**")
 
     let C = message.channel;
     message.guild.members.forEach((f, i) => {
-        if (f.id == 417403958814965771) return message.reply("Nemůžu zabanovat sebe, to doopravdy nejde.")
+        if (f.id == 417403958814965771) return message.reply(":x: **Nemůžu zabanovat sebe, od tohoto serveru!**")
         message.guild.member(f).ban(BReason);
-        message.channel.send(`Byl zabanován ${f}!`);
+        message.channel.send(`:white_check_mark: **Zabanován byl ${f}!**`);
     });
-    C.send("Všichni uživatele byly zabanováni!");
+    C.send(":white_check_mark: **Všichni uživatele byly zabanováni!**");
 }
 module.exports.help = {
     name: "banall",
