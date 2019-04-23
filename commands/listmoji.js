2315289f5889ea77974fa497e3c232d5c20c2441
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-23 07:00:56 +0200

    Update listmoji.js

--- commands/listmoji.js
+++ commands/listmoji.js
@@ -4,8 +4,9 @@ exports.run = (client, message) => {
   const emojiList = message.guild.emojis.map(e => e.toString()).join(' ');
   const embed = new RichEmbed()
     .setColor("RANDOM")
-    .setDescription(`${emojiList}\n\n**${message.guild.emojis.array()`);
-    .setFooter(`.length}** emotes jsou celkem na serveru.`)
+    .setTimestamp()
+    .setFooter(`**${message.guild.emojis.array().length}** emotes jsou celkem na serveru.`)
+    .setDescription(`${emojiList}`);
   message.channel.send({embed});
 }
 exports.help = {
