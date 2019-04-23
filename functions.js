3a0c25c1263c081124393791aed46dc32a3c6387
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-23 19:00:36 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -23,11 +23,6 @@ module.exports = (bot) => {
         jsfiles.forEach((f, i) => {
             let props = require(`./commands/${f}`);
             console.log(`${i + 1}: ${f} loaded!`);
-            bot.commands.set(props.help.name, props);
-            props.help.aliases.forEach(alias => {
-                bot.aliases.set(alias, props.help.name);
             });
         });
     });
-
-};
