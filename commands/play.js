ffe948fb6b7241366093fbdefc683410622213b3
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-23 18:44:04 +0200

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,105 @@
+const Discord = require("discord.js"); //Discord.js
+const ytdl = require("youtube-dl"); //Скчиваем видео
+const yts = require("youtube-search"); //Ищем видео
+
+async function play(connection, message, bot) {
+	var server = bot.queue[message.guild.id];
+	console.log(`${bot.user.username}: Сейчас играет: ${server.queue[0].title}`); //Лог
+	var description = server.queue[0].description.substring(0, 150) + '...';
+	
+	var videoinfo = new Discord.RichEmbed()
+		.setAuthor(server.queue[0].title, bot.user.avatarURL, server.queue[0].url)
+		.setColor("#ff3b3b")
+		.setThumbnail(server.queue[0].thumbnail)
+		.setDescription(`**Popisek:** ${description}`)
+		.setFooter("Dot | v1.5a");
+	await message.channel.send({embed: videoinfo}); //Отправляем embed
+	
+	var stream = ytdl(server.queue[0].id, ['-f', 'bestaudio[ext=m4a]']);
+	server.dispatcher = connection.playStream(stream, { volume: 0.3 });
+	
+	server.dispatcher.on("end", () => {
+		server.queue.shift();
+		
+		if(server.queue[0]) play(connection, message, bot);
+		else connection.disconnect();
+	})
+};
+
+module.exports.run = async (bot, message, args) => {
+	if (!args) {
+			message.channel.send(":x: **Zadej prosím odkaz nebo název hudby.**");
+	}
+
+	if (!message.member.voiceChannel) {
+			message.channel.send(":x: **Nejsi ve voice chatu!**");
+	}
+
+	if (!bot.queue[message.guild.id]) bot.queue[message.guild.id] = {
+			queue: []
+	}
+	
+	var server = bot.queue[message.guild.id]; //Получаем сервер
+	var ytlc = args.indexOf("youtube.com/watch") >= 0;
+	
+	//Если это ссылка
+	if(ytlc == true) {
+		ytdl.getInfo(args, ['-f', 'bestaudio[ext=m4a]'], (err, data) => {
+			if(err) console.log(err);
+			
+			server.queue.push({
+				"id": data.id,
+				"title": data.title,
+				"url": data.url,
+				"thumbnail": data.thumbnail,
+				"description": data.description
+			});
+			
+			if (!message.guild.voiceConnection) message.member.voiceChannel.join().then((connection) => {
+				play(connection, message, bot);
+			});
+		});
+	}
+
+	//Если это не ссылка
+	if(ytlc == false) {
+		yts(args, { maxResults: 1, key: process.env.ytkey }, (err, data) => {
+			if(err) console.log(err);
+			
+			data.map((data) => {
+				var ytcc = data.link.indexOf("youtube.com/channel") >= 0;
+				var ytpc = data.link.indexOf("youtube.com/playlist") >= 0;
+				
+				if(ytcc == true) {
+					message.channel.send(":x: **Myslím si, že tohle je na 100% kanál a ne hudba.**");
+				}
+				
+				if (ytpc == true) {;
+					message.channel.send(":x: **Toto je playlist, nemůžu ho pustit to nejde.** ;(");
+				}
+				
+				if (ytcc === false) {
+					if (ytpc === false) {
+						server.queue.push({
+							"id": data.id,
+							"title": data.title,
+							"url": data.link,
+							"thumbnail": data.thumbnails.high.url,
+							"description": data.description
+						});
+						
+						if (!message.guild.voiceConnection) message.member.voiceChannel.join().then((connection) => {
+							play(connection, message, bot);
+						})
+					}
+				}
+			})
+		})
+	}
+};
+
+module.exports.help = {
+	name: "play1"
+  aliases: []
+}
+
