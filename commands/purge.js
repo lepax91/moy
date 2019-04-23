b79d6ec96a4e8b00c34214a6c71546e183c42965
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-23 15:11:53 +0200

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
   var cleanEmbed = new Discord.RichEmbed()            
-            .setDescription(`Smazáno **${args[0]}** zpráv :white_check_mark:!`)
+            .setDescription(`**:white_check_mark: Smazáno **${args[0]}** zpráv!**`)
             .setFooter('Požadováno: ' + message.author.tag, message.author.avatarURL)
             .setTimestamp()
             .setColor('#ffffff');
