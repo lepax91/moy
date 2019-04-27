d1260d5627e8553fd127d44d75db48831bdea97b
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-27 11:11:23 +0200

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
 
-module.exports.run = async (bot, message, args) => {
+exports.run = async (bot, message, args) => {
 
     let bal = db.fetch(`money_${message.guild.id}_${message.author.id}`)
 
@@ -13,4 +13,8 @@ module.exports.run = async (bot, message, args) => {
     .setDescription(`Tvůj zůstatek je: `' + bal + '`)
     message.channel.send(embed)
 
+}
+exports.help = {
+    name: "balance69",
+    aliases: []
 }
