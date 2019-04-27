9830bb529e0b2cf081efea45bf640b32c49bdaca
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-27 14:51:22 +0200

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -5,15 +5,7 @@ const Discord = require('discord.js')
 
 exports.run = async (client, message, args, config) => {
 
-    let timeout = 21600000 // 24 hours in milliseconds, change if you'd like.
     
-    let daily = await db.fetch(`daily_${message.author.id}`);
-    
-    if (daily !== null && timeout - (Date.now() - daily) > 0) {
-        let time = ms(timeout - (Date.now() - daily));
-
-        message.channel.send(`✋ Uklidni se, musíš čekat 6 hodin! Už ti jenom zbývá celkem: **${time.hours}h ${time.minutes}m ${time.seconds}s**`)
-    } else {
     if (args[0] == 'hráč') {
 
         let amount = Math.floor(Math.random() * 500) + 1; // 1-500 random number. whatever you'd like
