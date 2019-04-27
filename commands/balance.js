d9a09d96bed87c8472d38a597f8fb99fd674ee57
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-27 09:42:51 +0200

    Create balance.js

---
+++ commands/balance.js
@@ -0,0 +1,36 @@
+const db = require('quick.db')
+const ms = require('parse-ms')
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args, config) => {
+
+
+    let timeout = 21600000 // 6 hours in milliseconds, change if you'd like.
+    let amount = 500
+    // random amount: Math.floor(Math.random() * 1000) + 1;
+
+
+    let daily = await db.fetch(`daily_${message.author.id}`);
+
+    if (daily !== null && timeout - (Date.now() - daily) > 0) {
+        let time = ms(timeout - (Date.now() - daily));
+
+        message.channel.send(`✋ Uklidni se! Musíš si počkat, Zbývají ti: **${time.hours}h ${time.minutes}m ${time.seconds}s**!`)
+    } else {
+    let embed = new Discord.RichEmbed()
+    .setTitle(`Daily`)
+    .setColor("GREEN")
+    .setDescription(`**Mám tu pro tebe daily, tady máš!**`)
+    .addField(`Získal jsi:`, amount)
+
+    message.channel.send(embed)
+    db.add(`money_${message.author.id}`, amount)
+    db.set(`daily_${message.author.id}`, Date.now())
+        
+    }
+
+}
+exports.help = {
+    name: "xdaily",
+    aliases: []
+}
