03649ccab177585247231a72186c8ef7681c3eb2
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-27 14:08:56 +0200

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,45 +1,37 @@
-module.exports.run = async (bot, message, args) => { // Run the command when a command is called
+const db = require('quick.db')
+const ms = require('parse-ms')
+const Discord = require('discord.js')
 
-    var discord = require('discord.js');
-    var currencyFormatter = require('currency-formatter')
-    var db = require('quick.db')
-    var ms = require('parse-ms');
-  
-   // This Code Is Registered To Zinx#9129
-   // Some Of This Code Has Been Altered To Work As Much As Possible
-    
-    let cooldown = 8.64e+7; // Cooldown Daily
-    let amount = 250; // Daily Payout
+exports.run = async (client, message, args, config) => {
 
-    let lastDaily = await db.fetch(`lastDaily_${message.author.id}`)
-    try {
-    db.fetch(`account_${message.member.id}`).then(bucks => {
-    if(bucks == null || 0){
-        db.set(`account_${message.member.id}`, 50)}
 
-    else if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0) {
-        let timeObj = ms(cooldown - (Date.now() - lastDaily))
+    let timeout = 21600000 // 24 hours in milliseconds, change if you'd like.
+    let amount = 500
+    // random amount: Math.floor(Math.random() * 1000) + 1;
 
-        let lastDailyEmbed = new discord.RichEmbed()
-        .setAuthor(`Author`, `Picture Here`)
-        .setColor(`DARK_RED`)
-        .setDescription(`Daily Not Ready. Please Wait **${timeObj.hours}h ${timeObj.minutes}m**!`)
-        message.channel.send(lastDailyEmbed)
+
+    let daily = await db.fetch(`daily_${message.author.id}`);
+
+    if (daily !== null && timeout - (Date.now() - daily) > 0) {
+        let time = ms(timeout - (Date.now() - daily));
+
+        message.channel.send("✋ Uklidni se, musíš čekat 6 hodin! Už ti jenom zbývá celkem: **${time.hours}h ${time.minutes}m ${time.seconds}s**`)
     } else {
-        db.set(`lastDaily_${message.author.id}`, Date.now());
-        db.add(`account_${message.member.id}`, amount).then(i => {
-            var discord = require('discord.js')
-            let dailyEmbed = new discord.RichEmbed()
-            .setAuthor(`Author Here`, `Picture Here`)
-            .setColor(`DARK_RED`)
-            .addField(`Account Holder: `, `${message.author}`)
-            .addField(`Daily Retrieved:`, `${currencyFormatter.format(amount, { code: 'USD' })}`)
-            message.channel.send(dailyEmbed)
-        })}
-    })} catch(err) {console.log(err)}
- }
+    let embed = new Discord.RichEmbed()
+    .setTitle("Daily")
+    .setColor("RANDOM")
+    .setDescription(`**Daily Reward**\n💸 Získal jsi celkem: amount!`)
+    .setFooter("</> v2.5a")
+    .setTimestamp()
 
-module.exports.help = { // This is the config for a command. Able to add things like proper usage & etc.
-    name: "eco.daily",
+    message.channel.send(embed)
+    db.add(`money_${message.author.id}`, amount)
+    db.set(`daily_${message.author.id}`, Date.now())
+        
+    }
+
+}
+exports.help = { // This is the config for a command. Able to add things like proper usage & etc.
+    name: "daily",
     aliases: []
 }
