6f65c589d985e53a26d8425bde76a70b094b8bcc
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 07:58:41 +0200

    Update magik.js

--- commands/magik.js
+++ commands/magik.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-    if (message.channel.id === "554326914622291968") {
         const low = require('lowdb')
         const FileSync = require('lowdb/adapters/FileSync')
         const adapter = new FileSync('./db.json')
@@ -10,9 +9,8 @@ exports.run = (client, message, args) => {
         var author = message.author.id;
        
     if (!db.get("credits").find({ auteur: author }).value()) {
-
         db.get("credits").push({ auteur: author, credits: 500 }).write()
- 
+ fa
         } else {
   
           var usercreditsdb = db.get("credits").filter({ auteur: author  }).find('credits').value()
@@ -21,13 +19,12 @@ exports.run = (client, message, args) => {
   
           db.get("credits").find({ auteur: author }).assign({ auteur: author, credits: credits[1] += 500 }).write()   
     }
-    message.channel.send("<:true:551460100347396107> ** patienter 2 heures avant de pouvoir effectuer encore cette action.**")   
-    }else {
-        message.channel.send("<:false:551460099600678944> **Vous devez effectuez cette action dans le salon daily du serveur officiel.**")
-    }
+    message.channel.send(":white_check_mark: **Právě jste získali 500 kreditů. Počkejte prosím 2 hodiny, než tuto akci provedete znovu.**")   
 
-    
+
+
 }
 module.exports.help = {
-    name: "daily"
+    name: "daily",
+    aliases: []
 }
