acb8d02eddc667ba2564e471911b9f3df11cda51
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 12:35:54 +0200

    Update setbio.js

--- commands/setbio.js
+++ commands/setbio.js
@@ -6,14 +6,14 @@ const db = low(adapter)
 
 exports.run = (client, message, args) => {
     var args = message.content.split(" ").slice(1);
-    var bio = args.join(" ")
+    var desc = args.join(" ")
     var author = message.author.id;
     if (!args[0]) return message.channel.send(":x: **Prosím, uveďte správný description svého profilu.**")
-    if (!db.get("bio").find({ auteur: author }).value()) {
-        db.get("bio").push({ auteur: author, field: bio }).write()
+    if (!db.get("description").find({ auteur: author }).value()) {
+        db.get("description").push({ auteur: author, description: desc }).write()
       } else {
-        db.get("bio").find({ auteur: author }).assign({ auteur: author, field: bio }).write()   
-}
+        db.get("description").find({ auteur: author }).assign({ auteur: author, description: desc }).write()   
+}  
 message.channel.send(":white_check_mark: Váš description byl úspěšně přidán do Profilu!**")
 }
 exports.help = {
