5ba2727746654559a2da7f4323d3ca93b8721763
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 21:10:19 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -6,15 +6,18 @@ exports.run = async (client, message, args) => {
 
   let bio = client.db.get(`bio_${member.id}`)
   let age = client.db.get(`age_${member.id}`)
+  let gay = client.db.get(`gay_${member.id}`)
   
   if(bio === null) bio = 'Nebylo zaznamenáno'
   if(age === null) age = 'Nebylo zaznamenáno'
+  if(gay === null) gay = 'Zatím ne 🏳️‍🌈'
   
   const embed = new Discord.RichEmbed()
   .setColor("RANDOM")
   .setTitle(`Profil: ${member.tag}`)
   .addField("🌎 Bio ", bio)
   .addField("🎂 Věk ", age)
+  .addField("🏳️‍🌈 Gay ", gay)
   .setFooter("</> Za 24 hodin, se všechno zrestuje.")
   message.channel.send(embed)
 }
