2ee13ee60f3dd788497e8e7f78a0043c1828d6ce
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 16:51:39 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -3,19 +3,17 @@ exports.run = (client, message, args, Discord) => {
   const member = message.mentions.users.first() || message.author;
   let money = client.db.get(`money_${member.id}-${message.guild.id}`)
   let bio = client.db.get(`bio_${member.id}`)
-  let age = client.db.get(`age_${member.id}`)
-  
+    
   if(money === null) money = 0
-  if(bio === null) bio = 'Nebylo zaznamenáno'
-  if(age === null) age = 'Nebylo zaznamenáno'
-
+  if(bio === null) bio = '**Nebylo zaznamenáno**'
+  
   const embed = client.embed
   embed.setColor("RANDOM")
   embed.setTitle(`👤 Profil: ${member.tag}`)
   embed.addField("🌎 Bio: ", bio)
-  embed.addField("🎂 Věk; ", age)
   embed.addField("💸 Peníze: ", money + '$')
   embed.setFooter("</> v2.5a")
+  embed.setTimestamp()
   
   message.channel.send(embed)
 };
