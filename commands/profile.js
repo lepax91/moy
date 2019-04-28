03fbebe9ee8d580366f9e56d989abdd0b759ebbd
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 19:22:16 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -3,20 +3,16 @@ exports.run = (client, message, args, Discord) => {
   const member = message.mentions.users.first() || message.author;
   let money = client.db.get(`money_${member.id}-${message.guild.id}`)
   let bio = client.db.get(`bio_${member.id}`)
-  let verified = client.db.get(`verify_${member.id}`)
   
   if(money === null) money = 0
   if(bio === null) bio = 'No bio set'
-  if(verified === null) verified = ':x:'
   
   const embed = new Discord.RichEmbed()
-  .setColor(client.color.MAIN)
+  .setColor("RANDOM")
   .setTitle(`Profile of ${member.tag}`)
-  .addField("Username:", member.username)
-  .addField("Verified", verified)
   .addField("Money: ", money + '$')
   .addField("Bio: ", bio)
-  .setFooter("ID: " + member.id)
+  .setFooter("</> v2.5a")
   
   message.channel.send(embed)
 };
