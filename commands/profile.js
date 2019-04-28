67783e1e940c97a207a9e6cd8f27c2c12c05e536
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 09:54:04 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -32,7 +32,7 @@ exports.run = (client, message, args) => {
         
         
           let profil = new Discord.RichEmbed()           
-             .setTitle("**_Profile_**)
+             .setTitle("**_Profile_**")
              .setDescription(desc)
              .addField("📝 Jméno", member.tag, true)
              .addField("💸 Peníze", credits + "$", true)
