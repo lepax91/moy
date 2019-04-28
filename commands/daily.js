317c0057fed5485a61366d9c56a3fe47aa61d0bf
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 20:18:22 +0200

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,35 +1,31 @@
-const parse = require('parse-ms')
+const db = require("quick.db");
+const ms = require("parse-ms");
 
-exports.run = async (client, message, args) => {
+exports.run = async (Bot, message, args) => {
+
     let coolDown = 10800000;
-    let amount = client.db.get(`dailyreward_${message.author.id}`)
-    
-    if(amount === null) amount = 250;
-  
-    if(client.db.get(`2x_${message.author.id}`) === true) amount = 500
-  
-    let lastDaily = await client.db.get(`lastDaily_${message.author.id}`);
+    let amount = 250;
+
+    let lastDaily = await db.get(`lastDaily_${message.author.id}`);
 
     if(lastDaily !== null && coolDown - (Date.now() - lastDaily) > 0) {
-        let timeObj  = parse(coolDown - (Date.now() - lastDaily));
+        let timeObj  = ms(coolDown - (Date.now() - lastDaily));
 
         message.channel.send({embed: {
-            title: "Error",
-            color: 0xff0000,
-            description: `**Prosím, vyzvedni si daily až za 3 hodiny.. Zbývá ti celkem: ${timeObj.hours}h and ${timeObj.minutes}m.**`
+            title: "Ups!",
+            color: 0xcc0000,
+            description: `:x: **Daily si můžeš vyzvednout každý 3 hodiny..** Zbývá ti: ${timeObj.hours}h a ${timeObj.minutes}m**`
         }});
     } else {
         message.channel.send({embed: {
             title: "Daily Reward",
-            color: 0x66ff99,
-            description: `:white_check_mark: **Daily byl úspěšně vyzvednutý, dostáváš 250$!**`
+            description: `Daily byl úspěšně vyzvednuty, dostáváš celkem: **$${amount}**. `
         }});
-      
-      
 
-        client.db.set(`lastDaily_${message.author.id}`, Date.now());
-        client.db.add(`money_${message.author.id}`, amount);
+        db.set(`lastDaily_${message.author.id}`, Date.now());
+        db.add(`userBalance_${message.author.id}`, 250);
     }
+
 };
 exports.help = {
     name: "daily",
