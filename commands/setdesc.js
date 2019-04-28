a5a74143680e01869c64acf26af6717e1ba66d05
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 10:33:59 +0200

    Update setdesc.js

--- commands/setdesc.js
+++ commands/setdesc.js
@@ -17,6 +17,6 @@ exports.run = (client, message, args) => {
 message.channel.send(":white_check_mark: Váš description byl úspěšně přidán do Profilu!**")
 }
 exports.help = {
-  name: "setdesc",
+  name: "setbio",
   aliases: []
 }
