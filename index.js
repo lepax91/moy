4608c8cd640a7d79c6790947ab6534531e4f2081
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-04-28 17:03:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const Discord = require("discord.js");
 const db = require('quick.db')
 const token = require("./token.json").token;
-const client = new Discord.Client({
+const client = new Discord.Client();
   disableEveryone: true
 });
 require("./functions")(client);
 
-client.db = db;
-client.embed = new Discord.RichEmbed()
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-client.afk = new Map();
-
+client.embed = new Discord.RichEmbed()
+client.config = config;
+client.db = db;
+client.color = {
+  'RED': '#ff0000',
+  'MAIN': '#ffcc66',
+  'GREEN': '#66ff99'
+}
 module.exports.bot = client;
 
 
