d76c36174a7b7cab3a04db2ea5a7cf30f180d62f
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-08 20:54:43 +0200

    Create setsex.js

---
+++ commands/setsex.js
@@ -0,0 +1,27 @@
+exports.run = (client, message, args) => {
+  
+  const embed = client
+  .setTimestamp()
+  
+  if(!args.join(" ")) {
+    
+    embed.setTitle("Error")
+    embed.setColor(client.color.RED)
+    embed.setDescription(":x: Chceš si napsat jakékoliv pohlaví prosím.. zadej pohlaví.. Příklad: ``.setsex <pohlaví>``")
+    
+    message.channel.send(embed)
+    return
+  };
+  
+  embed.setTitle("Tvoje pohlaví bylo úspěšně nastaveno")
+  embed.setColor("RANDOM")
+  embed.setDescription(":white_check_mark: Vaší pohlaví bylo nastaveno na: **" + args.join(" ") + "**")
+  
+  client.db.set(`sex_${message.author.id}`, args.join(" "))
+  
+  message.channel.send(embed)
+};
+exports.help = {
+  name: "setsex",
+  aliases: []
+}
