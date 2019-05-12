87b0366af5f4ae985818bdbd83f886f510042e43
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-12 19:32:59 +0200

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,62 +1,153 @@
 const Discord = require("discord.js");
- 
-exports.run = async (bot, message, args, tools) => {
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('You require a role with the permsissions \`Administrator\`').then(() => {
-        message.react(`⛔`);
-    });
-    let channel;
-    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {max:10});
-    const collector2 = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {max:10});
-    message.channel.send(`Giveaway setup started!\nWhat channel do you want the giveaway to take place in?(Mention the channel)\nNote giveaways last 1 day by default(Custom time functionality to be added in the future!)`);
-    collector.on(`collect`, col => {
-        if(!col.mentions.channels.first())return message.channel.send(`Please mention the giveaway channel!`);
-        channel = col.mentions.channels.first();
-        message.channel.send(`**Giveaway channel set to:**\n > <#${channel.id}>`);
-        message.channel.send(`What are you giving away?`);
-        const startColStop = () => {
-            setTimeout(endCol, 2000);
-        }
-        const endCol = () => {
-            collector.stop();
-        }
-        col.delete();
-        startColStop()
-    }),
-    collector.on('end', () => {
-        collector2.on("collect", async col2 => {
-            collector2.stop();
-            let givingAway = col2.content
-            if(givingAway === channel)return;
- 
-            message.channel.send(`**You are giving away:**\n > ${givingAway}`);
- 
-            let desc = `**What can you win?:**\n\n${givingAway}\n\n\n\n`
-            desc += `React with :tada: to enter!`
- 
-            let embed = new Discord.RichEmbed()
-            .setColor("#ff0000")
-            .setTitle(`Giveaway! by ${message.author.username}`)
-            .setDescription(desc)
-            let em = await channel.send(embed)
-            em.react(`🎉`)
-                     
-            const filter = (reaction, user) => reaction.emoji.name === '🎉'
-            const rCol = em.createReactionCollector(filter, { time: 5000});
- 
-            rCol.on(`collect`, r => {
-                    const end = async function() {
-                        let users = rCol.users.map(g => `${g.id}`).slice(1);
-                        let random = Math.floor(Math.random()* users.length);
-                        let winner = users[random];
-                        channel.send(`and the winner is **<@${winner}>**. Who won **${givingAway}**`);
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+  var room;
+  var title;
+  var duration;
+  var gMembers;const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+  var room;
+  var title;
+  var duration;
+  var gMembers;
+  var filter = m => m.author.id === message.author.id;
+ // if(message.content.startsWith(prefix + "giveaway")) {
+
+    if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **you should have "MANAGE GUILD" permission**');
+    message.channel.send(`:eight_pointed_black_star:| **in wich channel you want to start the giveaway?**`).then(msgg => {
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      }).then(collected => {
+        let room = message.guild.channels.find('name', collected.first().content);
+        if(!room) return message.channel.send(':heavy_multiplication_x:| **cannot find this channel**');
+        room = collected.first().content;
+        collected.first().delete();
+        msgg.edit(':eight_pointed_black_star:| **write time of the giveaway**').then(msg => {
+          message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 20000,
+            errors: ['time']
+          }).then(collected => {
+            if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **you must rewrite the command``write a correct time``**');
+            duration = collected.first().content * 60000;
+            collected.first().delete();
+            msgg.edit(':eight_pointed_black_star:| **whats the prize?**').then(msg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ['time']
+              }).then(collected => {
+                title = collected.first().content;
+                collected.first().delete();
+                try {
+                  let giveEmbed = new Discord.RichEmbed()
+                  .setAuthor(`Tanki Online`)
+                  .setTitle(title)
+                  .setDescription(`Time : ${duration / 60000} Minute`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+                  message.guild.channels.find('name', room).send(giveEmbed).then(m => {
+                     let re = m.react('🎉');
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users;
+                       let list = users.array().filter(u => u.id !== m.author.id);
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0];
+                         if(users.size === 1) gFilter = '**Not specified**';
+                       let endEmbed = new Discord.RichEmbed()
+                       .setAuthor(message.author.username, message.author.avatarURL)
+                       .setTitle(title)
+                       .addField('The giveaway ended !',`the winner is : ${gFilter}`)
+                       .setFooter(message.guild.name, message.guild.iconURL);
+                       m.edit(endEmbed);
+                     },duration);
+                   });
+                  msgg.edit(`:heavy_check_mark:| **the giveaway has been prepared**`);
+                } catch(e) {
+                  msgg.edit(`:heavy_multiplication_x:| **I dont have permissions**`);
+                  console.log(e);
                 }
-                setTimeout(end, 5000)
-            })
-           
-        })
-    })
-} 
-exports.help = {
-    name: 'test',
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+
+//JEBE MUUU
+
+  var filter = m => m.author.id === message.author.id;
+ // if(message.content.startsWith(prefix + "giveaway")) {
+
+    if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **you should have "MANAGE GUILD" permission**');
+    message.channel.send(`:eight_pointed_black_star:| **in wich channel you want to start the giveaway?**`).then(msgg => {
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      }).then(collected => {
+        let room = message.guild.channels.find('name', collected.first().content);
+        if(!room) return message.channel.send(':heavy_multiplication_x:| **cannot find this channel**');
+        room = collected.first().content;
+        collected.first().delete();
+        msgg.edit(':eight_pointed_black_star:| **write time of the giveaway**').then(msg => {
+          message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 20000,
+            errors: ['time']
+          }).then(collected => {
+            if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **you must rewrite the command``write a correct time``**');
+            duration = collected.first().content * 60000;
+            collected.first().delete();
+            msgg.edit(':eight_pointed_black_star:| **whats the prize?**').then(msg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ['time']
+              }).then(collected => {
+                title = collected.first().content;
+                collected.first().delete();
+                try {
+                  let giveEmbed = new Discord.RichEmbed()
+                  .setAuthor(`Tanki Online`)
+                  .setTitle(title)
+                  .setDescription(`Time : ${duration / 60000} Minute`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+                  message.guild.channels.find('name', room).send(giveEmbed).then(m => {
+                     let re = m.react('🎉');
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users;
+                       let list = users.array().filter(u => u.id !== m.author.id);
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0];
+                         if(users.size === 1) gFilter = '**Not specified**';
+                       let endEmbed = new Discord.RichEmbed()
+                       .setAuthor(message.author.username, message.author.avatarURL)
+                       .setTitle(title)
+                       .addField('The giveaway ended !',`the winner is : ${gFilter}`)
+                       .setFooter(message.guild.name, message.guild.iconURL);
+                       m.edit(endEmbed);
+                     },duration);
+                   });
+                  msgg.edit(`:heavy_check_mark:| **the giveaway has been prepared**`);
+                } catch(e) {
+                  msgg.edit(`:heavy_multiplication_x:| **I dont have permissions**`);
+                  console.log(e);
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+
+//JEBE MUUU
+
+module.exports.help = {
+    name: "test",
     aliases: []
-}             
+}
