658e05f16327d53c12cf692716c6d5052135dbdb
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-13 06:41:40 +0200

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,39 +1,36 @@
-const parse = require('parse-ms')	
-const db = require('quick-db')
+const db = require('quick.db')
+const ms = require('parse-ms')
+const Discord = require('discord.js')
 
- exports.run = async (client, message, args) => {	
-    let coolDown = 10800000;	
-    let amount = client.db.get(`dailyreward_${message.author.id}`)	
-
-     if(amount === null) amount = 100;	
-
-     if(client.db.get(`2x_${message.author.id}`) === true) amount = 200	
-
-     let lastDaily = await client.db.get(`lastDaily_${message.author.id}`);	
-
-     if(lastDaily !== null && coolDown - (Date.now() - lastDaily) > 0) {	
-        let timeObj  = parse(coolDown - (Date.now() - lastDaily));	
-
-        message.channel.send({embed: {	
-            title: "Error",
-            color: 0xff0000,	
-            description: `:x: Daily už sis vyzvednul! Musíš počkat 3 hodiny na další daily! Zbývá ti: **${timeObj.hours}h a ${timeObj.minutes}m**`	
-        }});	
-    } else {	
-        message.channel.send({embed: {	
-            title: "Daily Reward",	
-            color: 0x66ff99,	
-            description: `💵 Daily bylo úspěšně vyzvednuto, za 3 hodiny si zase přijď pro další!`	
- `	
-        }});	
+exports.run = async (client, message, args, config) => {
 
 
+    let timeout = 21600000 // 6 hours in milliseconds, change if you'd like.
+    let amount = 500
+    // random amount: Math.floor(Math.random() * 1000) + 1;
 
-         client.db.set(lastDaily_${message.author.id}, Date.now());
-         client.db.add(`money_${message.author.id}`, amount);	
-    }	
-};	
-exports.help = {	
-    name: "daily",	
-    aliases: []	
+
+    let money = await db.fetch(`daily_${message.author.id}`);
+
+    if (money !== null && timeout - (Date.now() - money) > 0) {
+        let time = ms(timeout - (Date.now() - money));
+
+        message.channel.send(`✋ Uklidni se! Musíš si počkat, Zbývají ti: **${time.hours}h ${time.minutes}m ${time.seconds}s**!`)
+    } else {
+    let embed = new Discord.RichEmbed()
+    .setTitle(`Daily Reward`)
+    .setColor("RANDOM")
+    .addField(`Dostal jsi celkem:`, amount)
+    .setTimestamp()
+
+    message.channel.send(embed)
+    db.add(`money_${message.author.id}`, money)
+    db.set(`daily_${message.author.id}`, Date.now())
+        
+    }
+
+}
+exports.help = {
+    name: "daily",
+    aliases: []
 }
