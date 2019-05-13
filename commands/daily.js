910f1f4dedd04b8fe08df016079ef04faa3cd8bc
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-13 06:30:37 +0200

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,1 +1,38 @@
+const parse = require('parse-ms')	
 
+ exports.run = async (client, message, args) => {	
+    let coolDown = 10800000;	
+    let amount = client.db.get(`dailyreward_${message.author.id}`)	
+
+     if(amount === null) amount = 100;	
+
+     if(client.db.get(`2x_${message.author.id}`) === true) amount = 200	
+
+     let lastDaily = await client.db.get(`lastDaily_${message.author.id}`);	
+
+     if(lastDaily !== null && coolDown - (Date.now() - lastDaily) > 0) {	
+        let timeObj  = parse(coolDown - (Date.now() - lastDaily));	
+
+        message.channel.send({embed: {	
+            title: "Error",
+            color: 0xff0000,	
+            description: `:x: Daily už sis vyzvednul! Musíš počkat 3 hodiny na další daily! Zbývá ti: **${timeObj.hours}h a ${timeObj.minutes}m**`	
+        }});	
+    } else {	
+        message.channel.send({embed: {	
+            title: "Daily Reward",	
+            color: 0x66ff99,	
+            description: `💵 Daily bylo úspěšně vyzvednuto, za 3 hodiny si zase přijď pro další!`	
+ `	
+        }});	
+
+
+
+         client.db.set(`lastDaily_${message.author.id}`, Date.now());	
+         client.db.add(`money_${message.author.id}`, amount);	
+    }	
+};	
+exports.help = {	
+    name: "daily",	
+    aliases: []	
+}
