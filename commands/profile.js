63476889435ace123f742b5da4389d4a075b4a8a
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-14 15:00:38 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -14,7 +14,7 @@ exports.run = async (client, message, args) => {
   if(job === null) job = 'Nebylo zaznamenáno'
   if(sex === null) sex = 'Nebylo zaznamenáno'
   
-  const embed = new Discord.RichEmbed()
+  const user = new Discord.RichEmbed()
   .setColor("RANDOM")
   .setTitle(`Profil: ${member.tag}`)
   .addField("🌎 Bio ", bio)
@@ -22,8 +22,33 @@ exports.run = async (client, message, args) => {
   .addField("♂️ Pohlaví", sex)
   .addField("👮 Povolání ", job)
   .setFooter("</> v1.5a | Dot Development Project")
-  message.channel.send(embed)
-}
+  message.channel.send(user)
+  
+        const embed = await message.channel.send(user)
+        if(message.member.presence.activity && message.member.presence.activity.name === 'Spotify') {
+            embed.react("🎧")
+        }
+
+        const filter = (reaction, user) => {
+            return '🎧'.includes(reaction.emoji.name) && user.id === message.author.id;
+        };
+
+        embed.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(collected => {
+            const reaction = collected.first();
+    
+            if (reaction.emoji.name === '🎧') {
+                const spotify = new Discord.MessageEmbed()
+                    .setTitle("🎧 Listening to Spotify 🎧")
+                    .setColor("RANDOM")
+                    .setThumbnail(message.member.presence.activity.assets.largeImageURL())
+                    .addField("💿 Název", message.member.presence.activity.details)
+                    .addField("🎼 Autor", `by ${message.member.presence.activity.state}`)
+                    .addField("💽 Album", message.member.presence.activity.assets.largeText)
+                embed.edit(spotify)
+                embed.reactions.removeAll()
+            
+});
 exports.help = {
     name: "profile",
     aliases: []
