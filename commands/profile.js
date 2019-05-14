f4fea480591e1135d9bb0bb211480a9cec643967
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-14 15:46:09 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js');
+var Vibrant = require('node-vibrant');
+const db = require("quick.db");
+
 
 exports.run = async (client, message, args) => {
 
   const member = message.mentions.users.first() || message.author;
+  const palette = await Vibrant.from(`https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=1024`).getPalette();
 
   let bio = client.db.get(`bio_${member.id}`)
   let age = client.db.get(`age_${member.id}`)
