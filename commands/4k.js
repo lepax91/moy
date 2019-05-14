85c0865bfde3b28697a73c25a5cae80d700e3e24
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-14 18:45:29 +0200

    Update 4k.js

--- commands/4k.js
+++ commands/4k.js
@@ -2,17 +2,7 @@ const Discord = require('discord.js');
 const { get } = require('snekfetch');
 
 exports.run = async (client, message) => {
-    
-    if (args[0] === 'help') {
-        const bye = new Discord.RichEmbed()
-        .setAuthor("Work help!")
-        .setDescription("Work types: \nconstructor \nprogrammer \nprostitute")
-        .setColor("GREEN")
-        .setFooter("V0.4alpha")
-        .setTimestamp();
-        message.channel.send(bye)
-        
-        
+            
     if (!message.channel.nsfw) {
     return message.channel.send(new Discord.RichEmbed()
       .setColor("RED")
