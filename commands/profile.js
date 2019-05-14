5d780c7bd886fa32d26ccc2522dc7187900bc2a9
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-14 17:51:24 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
-var Vibrant = require('node-vibrant');
-const db = require("quick.db");
+Vibrant = require('node-vibrant');
+quickdb = require('quick.db')
 
 
 exports.run = async (client, message, args) => {
 
-  const member = message.mentions.users.first() || message.author;
+  const member = message.mentions.members.size > 0 ? message.mentions.members.first() : message.member;
   const palette = await Vibrant.from(`https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=1024`).getPalette();
 
   let bio = client.db.get(`bio_${member.id}`)
@@ -18,14 +18,14 @@ exports.run = async (client, message, args) => {
             idle: `<:emoji_1:569849042545147914>`,
             dnd: `<:emoji_4:577846932794376214>`,
             offline: `<:emoji_5:577847052856459294>`
-          }         
+        } 
   const user = new Discord.RichEmbed()
   .setColor("RANDOM")
   .setTitle(`✨ User Profile`)
   .addField("🏷 Username", `**${message.author.username}**#${message.author.discriminator}`)
   .addField("🌎 Bio ", bio)
-  .addField("🎂 Věk", age)
-  .addField("⭐ Status", status[message.member.presence.status]                
+  .addField("🎂 Věk ", age)
+  .addField("⭐ Status", status[message.member.presence.status])
   .setFooter("v1.5a | Dot Development Project")
   message.channel.send(user)
 }                            
