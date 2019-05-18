ce747702dc1d8d64bdd74c95ea33bdfc8b7fccf2
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-18 21:44:54 +0200

    Update spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
-  if (message.author.id != 417403958814965771;577570268982018059) return message.channel.send("warning Příkaz mohou používat, kteří jsou v Dot Development Project.")
+  if (message.author.id != 417403958814965771,577570268982018059) return message.channel.send(":warning: Příkaz mohou používat, kteří jsou v Dot Development Project.")
   if (args.length !== 0) {
     if (message.content.indexOf("@everyone") > -1 || message.content.indexOf("@here") > -1) {
       message.channel.send("Promiň, ale nesmím spamovat everyone či here. :cry:");
