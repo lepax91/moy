d39259cd5bc5ea96f77803b46632df76b7c1d613
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-22 11:10:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const discord = require("discord.js");
+const Discord = require("discord.js");
+const db = require('quick.db')
 const token = require("./token.json").token;
-const bot = new discord.Client({
+const client = new Discord.Client({
   disableEveryone: true
 });
-require("./functions")(bot);
+require("./functions")(client);
+
+client.db = db;
+client.embed = new Discord.RichEmbed()
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.afk = new Map();
+
+module.exports.bot = client;
 
 
-bot.commands = new discord.Collection();
-bot.aliases = new discord.Collection();
-bot.afk = new Map();
-
-module.exports.bot = bot;
-
-bot.login(process.env.token); 
+client.login(process.env.token); 
