2b38cd33d7ba80e13e00d02a54b085361b78823d
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-26 20:13:32 +0200

    Create weather.js

---
+++ commands/weather.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, messaňůgege, args) => {
+    let apiKey = process.env.WEATHERAPI;
+    const fetch = require('node-fetch');
+    let arg = message.content.split(" ").join(" ").slice(10);
+    if (!arg) {
+        return message.reply(':warning: Požadováno město či vesnice, příkaz nebude fungovat.');
+    }
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + arg + `&APPID=` + apiKey + `&units=metric`)
+    .then(res =>{
+       return res.json(); 
+    }).then(json =>{
+        if (json.main === undefined) {
+            return message.reply(`:warning: **${arg}** není město ani vesnice.`);
+        }
+    let embed = new Discord.RichEmbed()
+    .setColor("#000FF")
+    .setTitle(`:flag_${json.sys.country.toLowerCase()}: ${json.name}`)
+    .addField(`🌦️ Stupně`, `${json.main.temp}°C`, true)
+    .addField("🌪️ Rychlost větru", `${json.wind.speed}MPH`, true)
+    .addField("🌬️ Vlhkost Vzduchu", `${json.main.humidity}%`, true)
+    .setFooter("Požadované od  " + message.author.username.author.avatarURL)
+    message.channel.send({embed})
+    .catch(console.error);
+}).catch(err => {
+    if (err) {
+        message.channel.send(':warning: Toto není chyba příkazu, chyba je v API.. Pokud chcete můžete kontaktovat <@417403958814965771>`);
+    }
+});
+};
+module.exports.help = {
+    name: "weather",
+    aliases: []
+}
