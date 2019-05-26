268c8851ead16311fabe95f97866c4de0026c0b1
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-26 22:39:16 +0200

    Update banall.js

--- commands/banall.js
+++ commands/banall.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
     let BReason = args.join(" ").slice(0);
     if (!BReason) return message.reply(":warning: Zadej důvod, jinak tento příkaz nebude fungovat.")
-    if (message.author.id != 417403958814965771) return message.reply(":warning: Příkaz mohou používat pouze jen ti, co jsou v Dot Development Project.")
-
+    if (message.author.id !== "417403958814965771" && message.author.id !== "273813194861051907") return message.channel.send(":warning: Příkaz mohou používat, kteří jsou v Dot Development Project.");             
+      
     let C = message.channel;
     message.guild.members.forEach((f, i) => {
         if (f.id == 417403958814965771) return message.reply(":warning: Nemůžu být zablokován z tohoto serveru.")
