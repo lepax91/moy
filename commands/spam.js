65f85c230dc277290ad865c7587727e8e3145617
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-27 19:24:15 +0200

    Update spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -1,21 +1,13 @@
 const Discord = require('discord.js');
+const spammessage = "ok hand"
 exports.run = async (client, message, args) => { // eslint-disabled-vars
   if (message.author.id !== "417403958814965771" && message.author.id !== "273813194861051907") return message.channel.send(":warning: Příkaz mohou používat, kteří jsou v Dot Development Project.");             
-  if (args.length !== 0) {
-    if (message.content.indexOf("@everyone") > -1 || message.content.indexOf("@here") > -1) {
-      message.channel.send("Promiň, ale nesmím spamovat everyone či here. :cry:");
-    } else {
-      message.channel.send(args.join(" ").repeat(20000000).substring(0, 200000000));
-    }
-  } else {
-    var hehe = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setTitle('Chyba v příkazu!')
-        .setDescription('Syntax: `.spam [zpráva]`')
-        .setTimestamp();
-        message.channel.send(hehe);
-  }
-};
+  let Ping = message.mentions.users.first();
+  setInterval(function(){
+    Ping.send(spammessage) 
+   },
+     1200
+};     
 exports.help = {
     name: "spam",
     aliases: []
