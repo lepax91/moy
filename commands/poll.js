0329dc9d449f3252f8846b5cd780f192dc03094a
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-05-28 22:14:38 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,6 +3,15 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
   if (args == 0) return message.channel.send('**_Chybí mi tu nějaká otázka 🤔_**')
 
+  if (args[0] === '') {
+        let embed = new Discord.RichEmbed()
+        .setTitle("🤔 Nevíš jak používat tento příkaz? Tento Help System ti to rád vysvětlí!")
+        .setDescription("👤 Pokud chceš udělat jakýsi hlasování, tak udělej pouze toto: `.poll [Otázka, a oni budou reagovat 👍 a 👎]`")
+        .setColor("GREEN")
+        .setFooter("⚠️ Pokud je nějaká chyba, kontaktujte Hlavního Vývojáře nebo Administraci.")
+        .setTimestamp();
+        message.channel.send(embed)
+  
   let embed = new Discord.RichEmbed()    
     .setTitle("Poll")
     .setColor("RANDOM")
