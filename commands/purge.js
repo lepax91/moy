acf7487d694af16a01d2492df35d9b1aadc59c3e
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-03 16:38:33 +0200

    Verze v2.5b: Upravená zpráva v if[args] + v des.

--- commands/purge.js
+++ commands/purge.js
@@ -3,10 +3,10 @@ var Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":warning: **Na tento příkaz musíš mít práva!**");
-  if(!args[0]) return message.channel.send("......");
+  if(!args[0]) return message.channel.send(":warning: Zadej číslo `1-100`, takhle to jinak nepůjde.");
   message.channel.bulkDelete(args[0]).then(() => {
   var cleanEmbed = new Discord.RichEmbed()            
-            .setDescription(`**:white_check_mark: Smazáno ${args[0]} zpráv!**`)
+            .setDescription(`**:white_check_mark: Smazal jsem ${args[0]} zpráv(u)!**`)
             .setFooter('Požadováno: ' + message.author.tag, message.author.avatarURL)
             .setTimestamp()
             .setColor('#ffffff');
