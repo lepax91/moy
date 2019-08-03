cd8c3ae66e5080cfa0c2db9b80c24d40b28762dd
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-03 15:11:20 +0200

    Verze 2.5b: Footer + Title + NSFW Check

--- commands/porngif.js
+++ commands/porngif.js
@@ -2,16 +2,20 @@ const Discord = require('discord.js');
 const { get } = require('snekfetch');
 
 exports.run = async (client, message) => {
-
-    if (!message.channel.nsfw) return message.channel.send(":underage: **_Tento příkaz je povolen jen kde je označen NSFW kanál!_**");
+if (!message.channel.nsfw) {
+    return message.channel.send(new Discord.RichEmbed()
+      .setColor("RED")
+      .setDescription(":underage: Tento příkaz je povolen jen kde je označen NSFW kanál.")
+    );
+  }      
     const { body } = await get("https://nekobot.xyz/api/image?type=pgif");
 
     const embed = new Discord.RichEmbed()
-        .setTitle('PornGif')
+        .setTitle('Pornografický Gif')
         .setURL(body.message)
         .setColor(0x619232)
         .setImage(body.message)
-        .setFooter(`Požadováno od: ${message.author.tag} | Verze: 1.5`)
+        .setFooter(`Požadováno od: ${message.author.tag} | Verze: 2.5b`)
         .setTimestamp();
     message.channel.send(embed);
 }
