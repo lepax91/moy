61af28bdfed5344abbe9819f437bc40552972498
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-05 18:46:09 +0200

    Update block.js

--- commands/block.js
+++ commands/block.js
@@ -3,10 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 var blockedUsers = [];
 
-if (blockedUsers.includes(message.author.id) || message.author.bot ||
 let user = message.mentions.users.first();
 if (user && !blockedUsers.includes(user.id)) blockedUsers.push(user.id);
-  
+if (blockedUsers.includes(message.author.id) || message.author.bot ||
 }
 module.exports.help = {
   name: "block",
