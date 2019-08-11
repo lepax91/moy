7efa2bb803123326037f8bccb4f6158b61f2b1e1
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 19:04:42 +0200

    Verze 2.5b: Upraven příkaz banall + error v embed.

--- commands/banall.js
+++ commands/banall.js
@@ -1,13 +1,19 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let BReason = args.join(" ").slice(0);
-    if (!BReason) return message.reply(":warning: Zadej důvod, jinak tento příkaz nebude fungovat.")
-    if (message.author.id !== "417403958814965771" && message.author.id !== "273813194861051907") return message.channel.send(":warning: Příkaz mohou používat, kteří jsou v Dot Development Project.");             
-      
+    if (message.author.id !== "417403958814965771" && message.author.id !== "273813194861051907") return message.channel.send(":warning: Příkaz mohou používat, kteří jsou v Dot Development Project.");
+    
+    const em = new Discord.RichEmbed()
+    .setTitle("Zadejte důvod, banall jde pouze z důvodem.")
+    .setTimestamp()
+    .setColor("RED")
+    .setFooter("2.5b")
+    return message.channel.send(em)    
+    
     let C = message.channel;
     message.guild.members.forEach((f, i) => {
-        if (f.id == 417403958814965771) return message.reply(":warning: Nemůžu být zablokován z tohoto serveru.")
+        if (f.id == 417403958814965771) return message.channel.send(":warning: Nemůžu být zablokován z tohoto serveru.")
         message.guild.member(f).ban(BReason);
         message.channel.send(`:white_check_mark: ${f} byl zablokován z tohoto serveru!`);
     });
