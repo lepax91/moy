15b8023031447922c45d5e9c4f000c09614363b2
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 22:07:17 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let botmessage = args.join(" ");
-  if (!botmessage) return.message.channel.send(">>> Zadejte nějaké slovo, bez slova tato akce nepůjde.")
+  if (!botmessage) return message.channel.send(">>> Zadejte nějaké slovo, bez slova tato akce nepůjde.")
   message.channel.send(botmessage);
 }
 
