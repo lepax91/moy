0b82c1c763998cbb595db48dfcdd7bd4f890b66b
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 22:19:45 +0200

    Update annoucement.js

--- commands/annoucement.js
+++ commands/annoucement.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   
 
    let botmessage = args.join(" ");
-   if (!botmessage) return message.channel.send(">>> Bez slova nemůžu udělat oznámení, zadejte nějaké slovo.");
+   if (!botmessage) return message.channel.send(">>> Musíte zadat nějaké slovo, bez slova akce **Annoucement** nepůjde.");
   
    var embed = new Discord.RichEmbed()
    .setDescription(botmessage)
