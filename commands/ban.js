6cf5076e3cb927e328e2c161e16822858502619b
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-12 00:14:49 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,17 +1,16 @@
 const fs = require('fs')
 const config = require('../config.json')
-const discord = require('discord.js')
+const Discord = require('discord.js');
 
 module.exports.run = (bot, message, args, discord) => {
     if (!message.member.hasPermission(["BAN_MEMBERS"])) return message.channel.send(">>> Na zabanovaní uživatele nemáš pravomoc, musíš mít nejprve pravomoc na zabanovaní uživatele!")
     let reason = args.slice(1).join(" ");
     let member = message.mentions.members.first();
     member.ban(reason).catch(console.error)
-    let embed = new discord.RichEmbed()
-    .setDescription(`:wave: Úspěšně byl zabanovaný ${member.displayName} :point_right:`)
+    const theirembed = new Discord.RichEmbed()
+    .setDescription(`:wave: Uživatel byl úspěšně zabanován ${member.displayName} :point_right:`)
     .setColor("RED")
-    .setThumbnail(bot.user.avatarURL)
-    message.channel.send(embed)
+    message.channel.send(theirembed)
     config.banreason = reason;
     fs.writeFile('../config.json', JSON.stringify(reason), (err) => console.error)
 }
