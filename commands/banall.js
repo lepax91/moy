5087f75046a6fe20c7444dc6d6c8a1abc9b00498
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 21:53:56 +0200

    Update banall.js

--- commands/banall.js
+++ commands/banall.js
@@ -2,16 +2,17 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let BReason = args.join(" ").slice(0);
-    if (!BReason) {
+    if (!BReason) 
     const em = new Discord.RichEmbed() 
     .setTitle("Tento příkaz funguje pouze s důvodem, zadejte prosím důvod aby zabanoval všechny uživatele.") 
     .setColor("RED") 
     return message.channel.send(em)    
     if (message.author.id !== "417403958814965771" && message.author.id !== "273813194861051907"); 
-    const em = new Discord.RichEmbed() .setTitle("Tento příkaz mohou používat pouze ti co jsou v Dot Development Project.") 
+    const em = new Discord.RichEmbed() 
+    .setTitle("Tento příkaz mohou používat pouze ti co jsou v Dot Development Project.") 
     .setColor("RED") 
     return message.channel.send(em) 
-    }  
+    
     let C = message.channel;
     message.guild.members.forEach((f, i) => {
         if (f.id == 417403958814965771) return message.channel.send(":warning: Nemůžu být zablokován z tohoto serveru.")
