a56bfa805c296e5c06cb11f007b5d0e830bed5a1
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 19:46:55 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let botmessage = args.join(" ");
+  const em = new Discord.RichEmbed()
+    .setTitle("Tento příkaz funguje pouze s důvodem, zadejte prosím důvod aby zabanoval všechny uživatele.")
+    .setColor("RED")
+    return message.channel.send(em)    
   message.channel.send(botmessage);
 }
 
