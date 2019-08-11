16258d82fde679477b493c29d8ecf9afb6bb5f23
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 21:42:58 +0200

    Update annoucement.js

--- commands/annoucement.js
+++ commands/annoucement.js
@@ -5,10 +5,7 @@ module.exports.run = async (bot, message, args) => {
   
 
    let botmessage = args.join(" ");
-   const em = new Discord.RichEmbed()
-    .setTitle("Musíte zadat nějaké slovo či větu, bez tohoto tato akce nepůjde.")
-    .setColor("RED")
-    return message.channel.send(em)    
+   if (!botmessage) const em = new Discord.RichEmbed() .setTitle("Musíte zadat nějaké slovo či větu, bez tohoto tato akce nepůjde.") .setColor("RED") return message.channel.send(em)    
   
    var embed = new Discord.RichEmbed()
    .setDescription(botmessage)
