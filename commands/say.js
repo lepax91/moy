936d4b79a16a7777c492e22240d7276d029227ac
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 22:06:12 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,9 +4,6 @@ module.exports.run = async (bot, message, args) => {
 
   let botmessage = args.join(" ");
   if (!botmessage) return.message.channel.send(">>> Zadejte nějaké slovo, bez slova tato akce nepůjde.")
-  const em = new Discord.RichEmbed()
-    .setColor("RED")
-    return message.channel.send(em)    
   message.channel.send(botmessage);
 }
 
