3dfa31bd51bfd84cc0477288d89425079df8f0e7
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-11 22:18:46 +0200

    Update banall.js

--- commands/banall.js
+++ commands/banall.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let BReason = args.join(" ").slice(0);
-    if (!BReason) return message.channel.send(">>> Tento příkaz funguje pouze s důvodem, zadejte ho aby akce fungovala.");
+    if (!BReason) return message.channel.send(">>> Tento příkaz funguje pouze s důvodem, zadejte důvod aby akce banall fungovala.");
     if (message.author.id !== "417403958814965771" && message.author.id !== "273813194861051907") return message.channel.send(">>> Tento příkaz mohou používat pouze ti kteří jsou v Dot Development Project.") 
     
     let C = message.channel;
