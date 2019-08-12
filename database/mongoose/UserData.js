aeb92b0fdf03c96ea25f9417553efc9bc249bc41
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-12 18:58:50 +0200

    Delete UserData.js

--- database/mongoose/UserData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const scheme = mongoose.Schema({
-  userId: String,
-  cash: { type: Number, default: 100 },
-  isBL: { type: Boolean, default: false }
-})
-
-module.exports = mongoose.model("UserData", scheme)
