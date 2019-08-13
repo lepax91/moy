85939419a67bb196f90464d59dc55ab56496a59d
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-08-13 14:56:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,14 +39,6 @@ bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 bot.afk = new Map();
 
-module.exports.bot = client;
+module.exports.bot = bot;
 
-  if (message.isMentioned("539139339741954099")) {
-    const em = new discord.RichEmbed()
-    .setDescription(`Zdravím jsem Dot! Do jakýhokoliv kánalu zadej příkaz ``.help`` aby jsi mohl vidět všechny moje příkazy!`)
-    .setColor("GREEN")
-    .setTimestamp()
-    message.channel.send({embed: em})
-      }
-  });
 bot.login(process.env.token); 
