97db20de8c4487a5d6e26a6040a511cdddd4fb12
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-09-15 14:26:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const discord = require("discord.js");
 const db = require('quick.db')
 const token = require("./token.json").token;	
 const bot = new discord.Client({	
