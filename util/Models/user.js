2c72e5c5201a2e36d37f4aefa7e3bc2c8c3099c1
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-09-15 14:19:30 +0200

    Delete user.js

--- util/Models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = mongoose.Schema({
-    user_id: String,
-    coins: Number,
-    blacklisted: {type: Boolean, default: false}
-})
-
-module.exports = mongoose.model("user", schema)
