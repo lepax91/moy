f999330bccf60510bfd4b69f285fc66e2459a945
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-09-16 15:11:18 +0200

    Create feet.js

---
+++ commands/feet.js
@@ -0,0 +1,31 @@
+const randomnsfw = require('random-puppy');
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+   	if (!message.channel.nsfw) {
+    return message.channel.send(new Discord.RichEmbed()
+      .setColor("RED")
+      .setDescription(":underage: Tento příkaz je povolen jen kde je označen NSFW kanál.")
+    );
+  }  
+	         let nsfwreddits = [
+        'feet',
+               'verifiedfeet'
+   
+    ]
+    let api = nsfwreddits[Math.round(Math.random() * (nsfwreddits.length - 1))];
+      randomnsfw(api).then(api => {
+           const theirembed = new Discord.RichEmbed()
+            .setTitle("Feet")
+            .setColor("RANDOM")
+            .setImage(api)
+            .setFooter(`Požadováno od: ${message.author.tag} | Verze: 2.5b`)
+            .setTimestamp();
+      message.channel.send(theirembed);
+      })
+}
+module.exports.help = {
+    name: "feet",
+    aliases: []
+}
